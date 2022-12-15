7d7aaf712964ead304b9661785cbf8856191dfab
Javier <javier@staff-net-vpn-dhcp-4513.intern.ethz.ch>
2022-12-15 11:45:29 +0100

    common cleanup

--- cli/common/get_booking_system_servers_list.sh
+++ cli/common/get_booking_system_servers_list.sh
@@ -3,7 +3,7 @@
 username=$(getent passwd ${SUDO_UID})
 username=${username%%:*}
 
-DB_IP=$(/opt/cli/common/get_booking_system_host) #129.132.186.30
-DB_USER=$(/opt/cli/common/get_booking_system_db_user) #alveo-vm # "{{ pam.mysql.user }}"
-DB_PASS=$(/opt/cli/common/get_booking_system_db_passwd) #koleno #"{{ pam.mysql.password }}"
+DB_IP=$(/opt/cli/common/get_booking_system_host) 
+DB_USER=$(/opt/cli/common/get_booking_system_db_user) 
+DB_PASS=$(/opt/cli/common/get_booking_system_db_passwd) 
 mysql -h $DB_IP -u $DB_USER -D alveo -p$DB_PASS -e "SELECT server.name FROM booking INNER JOIN booking_map ON booking.id = booking_map.booking_id INNER JOIN server ON booking_map.server_id = server.id WHERE now() BETWEEN booking.begin AND booking.end AND booking.status=1 AND user='$username';"
