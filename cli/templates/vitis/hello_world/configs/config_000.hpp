adcb6b60ca9517cc0f2db999baf6b818366b2445
Javier <javier@staff-net-vpn-dhcp-4961.intern.ethz.ch>
2023-01-30 13:42:02 +0100

    (vitis) VECTOR_LENGTH consolidation

--- cli/templates/vitis/hello_world/configs/config_000.hpp
+++ cli/templates/vitis/hello_world/configs/config_000.hpp
@@ -1,10 +1,9 @@
 //#if !defined(MYLIB_CONSTANTS_H)
 //#define MYLIB_CONSTANTS_H 1
 
-const int DATA_SIZE = 4096;
 const int T_clk = 10;
 const int W_MAX = 8;
-const int VECTOR_LENGTH = 4096;
+const int VECTOR_LENGTH = 16;
 const int FPGA_CLOCK_FREQUENCY = 400;
 const int W = 7;
 const int F = 0;
