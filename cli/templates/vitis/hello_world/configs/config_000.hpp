d7bd7ab115658e054d14c745bb6e69ed0fb552e3
Javier <javier@staff-net-vpn-dhcp-5480.intern.ethz.ch>
2023-02-07 08:39:25 +0100

    VECTOR_LENGTH replaced by N

--- cli/templates/vitis/hello_world/configs/config_000.hpp
+++ cli/templates/vitis/hello_world/configs/config_000.hpp
@@ -2,11 +2,10 @@
 //#define MYLIB_CONSTANTS_H 1
 
 const int T_clk = 10;
-const int W_MAX = 8;
-const int VECTOR_LENGTH = 16;
-const int FPGA_CLOCK_FREQUENCY = 400;
+const int N = 16;
 const int W = 7;
 const int F = 0;
+const int FPGA_CLOCK_FREQUENCY = 400;
 const double RMSE_MAX = 0.01;
 
 //#endif
