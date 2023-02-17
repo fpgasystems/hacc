521524b11f1462ed783396253cb2a8b5b8430d85
Javier <javier@staff-net-vpn-dhcp-7652.intern.ethz.ch>
2023-02-17 14:25:05 +0100

    Verified version

--- cli/new/mpi.sh
+++ cli/new/mpi.sh
@@ -29,12 +29,12 @@ while true; do
     DIR="/home/$username/my_projects/mpi/$project_name"
     if ! [ -d "$DIR" ]; then
         # project_name does not exist
-        mkdir ${DIR}
+        mkdir $DIR
         # clone repository
         #echo ""
         #echo "${bold}Checking out Coyote:${normal}"
         #echo ""
-        cd ${DIR}
+        #   cd ${DIR}
         #git clone https://github.com/fpgasystems/Coyote.git
         #mv Coyote/* .
         #rm -rf Coyote
@@ -44,8 +44,8 @@ while true; do
         #rm $DIR/sw/CMakeLists.txt
         #mv $DIR/CMakeLists.txt $DIR/sw
         #compile create config
-        #cd $DIR/src
-        #g++ -std=c++17 create_config.cpp -o ../create_config >&/dev/null
+        cd $DIR/src
+        g++ -std=c++17 create_config.cpp -o ../create_config >&/dev/null
         break
     fi
 done
