b594a5a46c37377e6170f7c322c35b5aed21f44b
Javier <javier@staff-net-vpn-dhcp-7652.intern.ethz.ch>
2023-02-17 13:03:37 +0100

    renaming to platform_params

--- cli/templates/mpi/hello_world/src/create_config.cpp
+++ cli/templates/mpi/hello_world/src/create_config.cpp
@@ -4,7 +4,7 @@
 #include <filesystem>
 #include <fstream> 
 #include <regex>
-#include "../mpi_params.hpp"
+#include "../platform_params.hpp"
 
 namespace fs = std::filesystem;
 using namespace std;
