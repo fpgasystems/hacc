d61f723a9b733625e067822f6be5ae94376ead74
Javier <javier@staff-net-als-0786.intern.ethz.ch>
2023-02-21 14:49:31 +0100

    dialogs verified for vitis

--- cli/build/vitis.sh
+++ cli/build/vitis.sh
@@ -9,66 +9,104 @@ username=$USER
 # inputs
 read -a flags <<< "$@"
 
-# flags cannot be empty
-if [ "$flags" = "" ]; then
-    /opt/cli/sgutil build vitis -h
-    exit
-fi
-
-project_found="0"
-serial_found="0"
-for (( i=0; i<${#flags[@]}; i++ ))
-do
-    if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --project " ]]; then # flags[i] is -p or --project
-        project_found="1"
-        project_idx=$(($i+1))
-        project_name=${flags[$project_idx]}
-    fi
-    if [[ " ${flags[$i]} " =~ " -s " ]] || [[ " ${flags[$i]} " =~ " --serial " ]]; then 
-        serial_found="1"
-        serial_idx=$(($i+1))
-        serial_number=${flags[$serial_idx]}
-    fi
-done
-
-# mandatory flags (-p must be used)
-use_help="0"
-if [[ $project_found = "0" ]]; then
-    use_help="1"
-fi
-
-# forbiden combinations (serial_found and target_found only make sense with project_found = 1)
-if [[ $project_found = "0" ]] && [[ $serial_found = "1" ]]; then
-    use_help="1"
-fi
-
-#print help
-if [[ $use_help = "1" ]]; then
-    /opt/cli/sgutil build vitis -h
-    exit
-fi
-
-# when used, project_name or serial_found cannot be empty
-if [ "$project_found" = "1" ] && [ "$project_name" = "" ]; then
-    /opt/cli/sgutil build vitis -h
-    exit
-fi
-
-if [ "$serial_found" = "1" ] && [ "$serial_number" = "" ]; then
-    /opt/cli/sgutil build vitis -h
-    exit
-fi
-
-DIR="/home/$username/my_projects/vitis/$project_name"
-if ! [ -d "$DIR" ]; then
-    echo ""
-    echo "$DIR is not a valid --project name!"
-    echo ""
-    exit
-fi
-
 echo ""
 echo "${bold}sgutil build vitis${normal}"
+
+#check on flags (before: flags cannot be empty)
+project_found="0"
+serial_found="0"
+if [ "$flags" = "" ]; then
+    #no flags: start dialog
+    cd /home/$username/my_projects/vitis/
+    projects=( *"/" )
+    #delete common from projects
+    j=0
+    for i in "${projects[@]}"
+    do
+        if [[ $i =~ "common/" ]]; then
+            echo "" >&/dev/null
+        else
+            aux[j]=$i
+            j=$(($j + 1))
+        fi
+    done
+    echo ""
+    echo "${bold}Please, choose your project:${normal}"
+    echo ""
+    PS3=""
+    select project_name in "${aux[@]}"; do #projects
+        if [[ -z $project_name ]]; then
+            echo "" >&/dev/null
+        else
+            project_found="1"
+            project_name=${project_name::-1} #we remove the last character, i.e. "/""
+            break
+        fi
+    done
+else
+    #find flags and values
+    for (( i=0; i<${#flags[@]}; i++ ))
+    do
+        if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --project " ]]; then # flags[i] is -p or --project
+            project_found="1"
+            project_idx=$(($i+1))
+            project_name=${flags[$project_idx]}
+        fi
+        if [[ " ${flags[$i]} " =~ " -s " ]] || [[ " ${flags[$i]} " =~ " --serial " ]]; then 
+            serial_found="1"
+            serial_idx=$(($i+1))
+            serial_number=${flags[$serial_idx]}
+        fi
+    done
+    #forbidden combinations
+    if [[ $project_found = "0" ]] || ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $serial_found = "1" ]) || ([ "$serial_found" = "1" ] && [ "$serial_number" = "" ]); then
+        /opt/cli/sgutil build vitis -h
+        exit
+    fi
+fi
+
+
+#----------------------
+
+
+# mandatory flags (-p must be used)
+#use_help="0"
+#if [[ $project_found = "0" ]]; then
+#    use_help="1"
+#fi
+
+# forbiden combinations (serial_found and target_found only make sense with project_found = 1)
+#if [[ $project_found = "0" ]] && [[ $serial_found = "1" ]]; then
+#    use_help="1"
+#fi
+
+#print help
+#if [[ $use_help = "1" ]]; then
+#    /opt/cli/sgutil build vitis -h
+#    exit
+#fi
+
+# when used, project_name or serial_found cannot be empty
+#if [ "$project_found" = "1" ] && [ "$project_name" = "" ]; then
+#    /opt/cli/sgutil build vitis -h
+#    exit
+#fi
+
+#if [ "$serial_found" = "1" ] && [ "$serial_number" = "" ]; then
+#    /opt/cli/sgutil build vitis -h
+#    exit
+#fi
+
+#define directories (1)
+DIR="/home/$username/my_projects/vitis/$project_name"
+
+#check for project directory
+if ! [ -d "$DIR" ]; then
+    echo ""
+    echo "You must create your project first! Please, use sgutil new vitis"
+    echo ""
+    exit
+fi
 
 #create or select a configuration
 cd $DIR/configs/
@@ -129,13 +167,16 @@ if [ $((n + 0)) -eq  1 ]; then
     platform=$(echo *)
 fi
 
-DIR="/home/$username/my_projects/vitis/$project_name"
-if ! [ -d "$DIR" ]; then
-    echo ""
-    echo "$DIR not found!"
-    echo ""
-    exit
-else
+#define directories (2)
+APP_BUILD_DIR="/home/$username/my_projects/vitis/$project_name/build_dir.$target.$platform"
+
+#DIR="/home/$username/my_projects/vitis/$project_name"
+#if ! [ -d "$DIR" ]; then
+#    echo ""
+#    echo "$DIR not found!"
+#    echo ""
+#    exit
+#else
     echo ""
     echo "${bold}Changing directory:${normal}"
     echo ""
@@ -144,9 +185,9 @@ else
     cd $DIR
 
     #compilation
-    BUILD_DIR="/home/$username/my_projects/vitis/$project_name/build_dir.$target.$platform" # build_dir.hw.xilinx_u55c_gen3x16_xdma_3_202210_1
-    if ! [ -d "$BUILD_DIR" ]; then
-        # BUILD_DIR does not exist
+    #APP_BUILD_DIR="/home/$username/my_projects/vitis/$project_name/build_dir.$target.$platform" # build_dir.hw.xilinx_u55c_gen3x16_xdma_3_202210_1
+    if ! [ -d "$APP_BUILD_DIR" ]; then
+        # APP_BUILD_DIR does not exist
         export CPATH="/usr/include/x86_64-linux-gnu" #https://support.xilinx.com/s/article/Fatal-error-sys-cdefs-h-No-such-file-or-directory?language=en_US
         echo "${bold}PL kernel compilation and linking: generating .xo and .xclbin:${normal}"
         echo ""
@@ -159,7 +200,7 @@ else
         echo ""
         echo "make all TARGET=$target PLATFORM=$platform" 
         echo ""
-        echo "$BUILD_DIR already exists!"
+        echo "$APP_BUILD_DIR already exists!"
         echo ""
     fi
-fi
+#fi
--- cli/program/vitis.sh
+++ cli/program/vitis.sh
@@ -9,64 +9,111 @@ username=$USER
 # inputs
 read -a flags <<< "$@"
 
-# flags cannot be empty
-if [ "$flags" = "" ]; then
-    /opt/cli/sgutil program vitis -h
-    exit
-fi
+echo ""
+echo "${bold}sgutil program vitis${normal}"
 
+#check on flags (before: flags cannot be empty)
 project_found="0"
 serial_found="0"
-for (( i=0; i<${#flags[@]}; i++ ))
-do
-    if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --project " ]]; then # flags[i] is -p or --project
-        project_found="1"
-        project_idx=$(($i+1))
-        project_name=${flags[$project_idx]}
+if [ "$flags" = "" ]; then
+    #no flags: start dialog
+    cd /home/$username/my_projects/vitis/
+    projects=( *"/" )
+    #delete common from projects
+    j=0
+    for i in "${projects[@]}"
+    do
+        if [[ $i =~ "common/" ]]; then
+            echo "" >&/dev/null
+        else
+            aux[j]=$i
+            j=$(($j + 1))
+        fi
+    done
+    echo ""
+    echo "${bold}Please, choose your project:${normal}"
+    echo ""
+    PS3=""
+    select project_name in "${aux[@]}"; do #projects
+        if [[ -z $project_name ]]; then
+            echo "" >&/dev/null
+        else
+            project_found="1"
+            project_name=${project_name::-1} #we remove the last character, i.e. "/""
+            break
+        fi
+    done
+else
+    #find flags and values
+    for (( i=0; i<${#flags[@]}; i++ ))
+    do
+        if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --project " ]]; then # flags[i] is -p or --project
+            project_found="1"
+            project_idx=$(($i+1))
+            project_name=${flags[$project_idx]}
+        fi
+        if [[ " ${flags[$i]} " =~ " -s " ]] || [[ " ${flags[$i]} " =~ " --serial " ]]; then 
+            serial_found="1"
+            serial_idx=$(($i+1))
+            serial_number=${flags[$serial_idx]}
+        fi
+    done
+    #forbidden combinations
+    if [[ $project_found = "0" ]] || ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $serial_found = "1" ]) || ([ "$serial_found" = "1" ] && [ "$serial_number" = "" ]); then
+        /opt/cli/sgutil build vitis -h
+        exit
     fi
-    if [[ " ${flags[$i]} " =~ " -s " ]] || [[ " ${flags[$i]} " =~ " --serial " ]]; then # =====> will be --fpga 0
-        serial_found="1"
-        serial_idx=$(($i+1))
-        serial_number=${flags[$serial_idx]}
-    fi
-done
-
-# mandatory flags (-p must be used)
-use_help="0"
-if [[ $project_found = "0" ]]; then
-    use_help="1"
 fi
 
-# forbiden combinations (serial_found and target_found only make sense with project_found = 1)
-if [[ $project_found = "0" ]] && [[ $serial_found = "1" ]]; then
-    use_help="1"
-fi
-
-#print help
-if [[ $use_help = "1" ]]; then
-    /opt/cli/sgutil program vitis -h
-    exit
-fi
-
-# when used, project_name or serial_found cannot be empty
-if [ "$project_found" = "1" ] && [ "$project_name" = "" ]; then
-    /opt/cli/sgutil program vitis -h
-    exit
-fi
-
-if [ "$serial_found" = "1" ] && [ "$serial_number" = "" ]; then
-    /opt/cli/sgutil program vitis -h
-    exit
-fi
-
-# verify directory
+#define directories (1)
 DIR="/home/$username/my_projects/vitis/$project_name"
+
+#check if project exists
 if ! [ -d "$DIR" ]; then
     echo ""
     echo "$DIR is not a valid --project name!"
     echo ""
     exit
 fi
+
+#create or select a configuration
+cd $DIR/configs/
+if [[ $(ls -l | wc -l) = 2 ]]; then
+    #only config_000 exists and we create config_001
+    #we compile create_config (in case there were changes)
+    #cd $DIR/src
+    #g++ -std=c++17 create_config.cpp -o ../create_config >&/dev/null
+    #cd $DIR
+    #./create_config
+    #cp -fr $DIR/configs/config_001.hpp $DIR/configs/config_000.hpp
+    config=""
+    echo "" >&/dev/null
+elif [[ $(ls -l | wc -l) = 3 ]]; then
+    #config_000 and config_001 exist
+    cp -fr $DIR/configs/config_001.hpp $DIR/configs/config_000.hpp
+    config="config_001.hpp"
+    echo ""
+elif [[ $(ls -l | wc -l) > 3 ]]; then
+    cd $DIR/configs/
+    configs=( "config_"*.hpp )
+    echo ""
+    echo "${bold}Please, choose your configuration:${normal}"
+    echo ""
+    PS3=""
+    select config in "${configs[@]:1}"; do # with :1 we avoid config_000.hpp :${#configs[@]}-2
+        if [[ -z $config ]]; then
+            echo "" >&/dev/null
+        else
+            break
+        fi
+    done
+    # copy selected config as config_000.hpp
+    cp -fr $DIR/configs/$config $DIR/configs/config_000.hpp
+    echo ""
+fi
+
+#set execution target
+target="hw"
 
 #sgutil get serial only when we have one FPGA and not serial_found
 if [[ $(lspci | grep Xilinx | wc -l) = 1 ]] & [[ $serial_found = "0" ]]; then
@@ -81,33 +128,34 @@ if [ $((n + 0)) -eq  1 ]; then
     platform=$(echo *)
 fi
 
-#change directory
-if ! [ -d "$DIR" ]; then
-    echo ""
-    echo "$DIR not found!"
-    echo ""
-    exit
-else
-    echo ""
-    echo "${bold}Changing directory:${normal}"
-    echo ""
-    echo "cd $DIR"
-    echo ""
-    cd $DIR
-fi
+#define directories (2)
+APP_BUILD_DIR="/home/$username/my_projects/vitis/$project_name/build_dir.$target.$platform"
 
-# verify hw target 
-target="hw"
-DIR="/home/$username/my_projects/vitis/$project_name/build_dir.$target.$platform"
-if ! [ -d "$DIR" ]; then
-    # project_name does not exist
-    echo "Please, generate your hw binary first with sgutil build vitis."
+#change directory
+#if ! [ -d "$DIR" ]; then
+#    echo ""
+#    echo "$DIR not found!"
+#    echo ""
+#    exit
+#else
+    #echo ""
+    #echo "${bold}Changing directory:${normal}"
+    #echo ""
+    #echo "cd $DIR"
+    #echo ""
+    #cd $DIR
+#fi
+
+#check for build directory
+if ! [ -d "$APP_BUILD_DIR" ]; then
+    #echo ""
+    echo "You must generate your application first! Please, use sgutil build vitis (target = hw)"
     echo ""
     exit
 fi
 
 # get xclbin
-cd $DIR
+cd $APP_BUILD_DIR
 xclbin=$(echo *.xclbin | awk '{print $NF}')
 
 # program xclbin
