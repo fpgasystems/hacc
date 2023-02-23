e0b5b4e0a6b644ee53ec9a7bcc38e36fd14fb2bd
Javier <javier@staff-net-vpn-dhcp-0574.intern.ethz.ch>
2023-02-23 13:57:40 +0100

    Adding elif for virtualized and hot_plug

--- cli/program/vivado.sh
+++ cli/program/vivado.sh
@@ -4,6 +4,7 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 # constants
+EMAIL="jmoyapaya@ethz.ch"
 SERVERADDR="localhost"
 
 #get username
@@ -122,10 +123,22 @@ if [[ $program_bitstream = "1" ]]; then
 	echo "${bold}Programming bitstream:${normal}"
     /tools/Xilinx/Vivado/${branch:7:6}/bin/vivado -nolog -nojournal -mode batch -source /opt/cli/program/flash_bitstream.tcl -tclargs $SERVERADDR $serial_number $device_name $bit_file $ltx_file
 
-    #hotplug
-    if [[ $(lspci | grep Xilinx | wc -l) = 2 ]]; then
-        #sudo bash -c "source /opt/cli/program/pci_hot_plug ${hostname}"
-        sudo /opt/cli/program/pci_hot_plug ${hostname}
+    #check for virtualized and apply PCI hot plug
+    virtualized=$(/opt/cli/common/is_virtualized)
+    if [ "$virtualized" = "true" ]; then
+        echo "${bold}The server needs to warm boot to operate in Vivado workflow. For this purpose:${normal}"
+		echo ""
+		echo "    Use the ${bold}go to baremetal${normal} button on the booking system, or"
+		echo "    Contact ${bold}$EMAIL${normal} for support."
+        echo ""
+        #send email
+        echo "Subject: $username requires to warm boot/go to baremetal ($hostname)" | sendmail $EMAIL
+        exit
+    elif [ "$virtualized" = "false" ]; then
+        #run PCI hot plug
+        if [[ $(lspci | grep Xilinx | wc -l) = 2 ]]; then
+            sudo /opt/cli/program/pci_hot_plug ${hostname}
+        fi
     fi
 fi
 
