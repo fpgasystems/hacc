7fa48ea0d9f44984e644d24de3db9acd31f857a2
Javier <javier@staff-net-vpn-dhcp-6822.intern.ethz.ch>
2023-05-11 15:01:44 +0200

    Working version

--- cli/validate/vitis.sh
+++ cli/validate/vitis.sh
@@ -3,6 +3,10 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+#constants
+CLI_WORKDIR="/opt/cli"
+MAX_DEVICES=4
+
 #get username
 username=$USER
 
@@ -25,10 +29,87 @@ hostname="${url%%.*}"
 # inputs
 read -a flags <<< "$@"
 
-#define directories
-CLI_WORKDIR="/opt/cli"
+echo ""
+echo "${bold}sgutil validate vitis${normal}"
+echo ""
 
+#check on multiple Xilinx devices
+if [[ -z $(lspci | grep Xilinx) ]]; then
+    multiple_devices=""
+    echo "No Xilinx device found."
+    echo ""
+    exit
+elif [[ $(lspci | grep Xilinx | wc -l) = 2 ]]; then
+    #servers with only one FPGA (i.e., alveo-u55c-01)
+    multiple_devices="0"
+elif [[ $(lspci | grep Xilinx | wc -l) -gt 2 ]]; then
+    #servers with eight FPGAs (i.e., alveo-u280)
+    multiple_devices="1"
+else
+    echo "Unexpected number of Xilinx devices."
+    echo ""
+    exit
+fi
 
+echo "multiple_devices=$multiple_devices"
+
+#check on flags
+device_found=""
+device_index=""
+if [ "$flags" = "" ]; then
+
+    echo "no flags"
+
+    #get device index
+    if [[ "$multiple_devices" == "0" ]]; then
+        #servers with only one FPGA (i.e., alveo-u55c-01)
+        device_index="0"
+    elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
+        #servers with four FPGAs (i.e., hacc-box-01)
+        echo "hola!"
+        #we need to ask for the index
+        #0
+        #1
+        #2
+        #3
+
+    fi
+else
+
+    echo "flags"
+
+    #find flags and values
+    for (( i=0; i<${#flags[@]}; i++ ))
+    do
+        if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
+            device_found="1"
+            device_idx=$(($i+1))
+            device_index=${flags[$device_idx]}
+        fi  
+    done
+
+    echo "device_found=$device_found"
+    echo "device_idx=$device_idx"
+    echo "device_index=$device_index"
+
+    #forbidden combinations
+    if [[ $device_found = "0" ]] || [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 0 ))); then
+        #$CLI_WORKDIR/sgutil validate vitis -h
+        echo "Forbidden comb."
+        /opt/cli/sgutil validate vitis -h
+        exit
+    fi
+fi
+
+exit
+
+#quan busquem device_index i és major que MAX_DEVICES
+
+#get BDF from device_idx
+bdf="xxx"
+
+#validate
+/opt/xilinx/xrt/bin/xbutil validate --device $bdf
 
 
 
