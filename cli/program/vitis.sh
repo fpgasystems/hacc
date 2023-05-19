86d19fa054a4513d4e905c9c4779e3e22cb8668c
Javier <javier@staff-net-vpn-dhcp-6327.intern.ethz.ch>
2023-05-19 10:37:44 +0200

    Verified for reading BFD from database

--- cli/program/vitis.sh
+++ cli/program/vitis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+CLI_WORKDIR="/opt/cli"
+DATABASE="/opt/hacc/devices"
+
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -27,9 +30,24 @@ if ! [ -d "/home/$username/my_projects/vitis/" ]; then
     exit
 fi
 
+#check on multiple Xilinx devices
+num_devices=$(/opt/cli/common/get_num_devices)
+if [[ -z "$num_devices" ]] || [[ "$num_devices" -eq 0 ]]; then
+    echo ""
+    echo "Please, update $DATABASE according to your infrastructure."
+    echo ""
+    exit
+elif [[ "$num_devices" -eq 1 ]]; then
+    multiple_devices="0"
+else
+    multiple_devices="1"
+fi
+
 #check on flags (before: flags cannot be empty)
 project_found="0"
-serial_found="0"
+device_found=""
+device_index=""
+#serial_found="0"
 if [ "$flags" = "" ]; then
     #no flags: start dialog
     cd /home/$username/my_projects/vitis/
@@ -58,6 +76,49 @@ if [ "$flags" = "" ]; then
             break
         fi
     done
+    #get device index
+    if [[ "$multiple_devices" == "0" ]]; then
+        #servers with only one FPGA (i.e., alveo-u55c-01)
+        device_index="0"
+    elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
+        #servers with four FPGAs (i.e., hacc-box-01)
+        #device_0
+        id_0=$($CLI_WORKDIR/get/get_device_param 0 id)
+        device_type_0=$($CLI_WORKDIR/get/get_device_param 0 device_type)
+        device_name_0=$($CLI_WORKDIR/get/get_device_param 0 device_name)
+        serial_number_0=$($CLI_WORKDIR/get/get_device_param 0 serial_number)
+        #device_1
+        id_1=$($CLI_WORKDIR/get/get_device_param 1 id)
+        device_type_1=$($CLI_WORKDIR/get/get_device_param 1 device_type)
+        device_name_1=$($CLI_WORKDIR/get/get_device_param 1 device_name)
+        serial_number_1=$($CLI_WORKDIR/get/get_device_param 1 serial_number)
+        #device_2
+        id_2=$($CLI_WORKDIR/get/get_device_param 2 id)
+        device_type_2=$($CLI_WORKDIR/get/get_device_param 2 device_type)
+        device_name_2=$($CLI_WORKDIR/get/get_device_param 2 device_name)
+        serial_number_2=$($CLI_WORKDIR/get/get_device_param 2 serial_number)
+        #device_3
+        id_3=$($CLI_WORKDIR/get/get_device_param 3 id)
+        device_type_3=$($CLI_WORKDIR/get/get_device_param 3 device_type)
+        device_name_3=$($CLI_WORKDIR/get/get_device_param 3 device_name)
+        serial_number_3=$($CLI_WORKDIR/get/get_device_param 3 serial_number)
+        #concatenate strings
+        devices=( "$id_0 [$device_type_0 - $device_name_0 - $serial_number_0]" "$id_1 [$device_type_1 - $device_name_1 - $serial_number_1]" "$id_2 [$device_type_2 - $device_name_2 - $serial_number_2]" "$id_3 [$device_type_3 - $device_name_3 - $serial_number_3]")
+        #multiple choice
+        echo ""
+        echo "${bold}Please, choose your device:${normal}"
+        echo ""
+        PS3=""
+        select device_index in "${devices[@]}"; do
+            if [[ -z $device_index ]]; then
+                echo "" >&/dev/null
+            else
+                device_found="1"
+                device_index=${device_index:0:1}
+                break
+            fi
+        done
+    fi
 else
     #find flags and values
     for (( i=0; i<${#flags[@]}; i++ ))
@@ -67,15 +128,22 @@ else
             project_idx=$(($i+1))
             project_name=${flags[$project_idx]}
         fi
-        if [[ " ${flags[$i]} " =~ " -s " ]] || [[ " ${flags[$i]} " =~ " --serial " ]]; then 
-            serial_found="1"
-            serial_idx=$(($i+1))
-            serial_number=${flags[$serial_idx]}
-        fi
+        for (( i=0; i<${#flags[@]}; i++ ))
+        do
+            if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
+                device_found="1"
+                device_idx=$(($i+1))
+                device_index=${flags[$device_idx]}
+            fi  
+        done
     done
     #forbidden combinations
-    if [[ $project_found = "0" ]] || ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $serial_found = "1" ]) || ([ "$serial_found" = "1" ] && [ "$serial_number" = "" ]); then
-        /opt/cli/sgutil build vitis -h
+    if [[ $project_found = "0" ]] || ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $device_found = "1" ]) || ([ "$device_found" = "1" ] && [ "$device_index" = "" ]); then
+        /opt/cli/sgutil program vitis -h
+        exit
+    fi
+    if [[ $device_found = "0" ]] || [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 0 ))); then
+        $CLI_WORKDIR/sgutil program vitis -h
         exit
     fi
 fi
@@ -124,11 +192,17 @@ fi
 #fi
 
 # serial to platform
-cd /opt/xilinx/platforms
-n=$(ls -l | grep -c ^d)
-if [ $((n + 0)) -eq  1 ]; then
-    platform=$(echo *)
-fi
+#cd /opt/xilinx/platforms
+#n=$(ls -l | grep -c ^d)
+#if [ $((n + 0)) -eq  1 ]; then
+#    platform=$(echo *)
+#fi
+
+#get platform
+platform=$(/opt/cli/get/get_device_param $device_index platform)
+
+#get device name
+#device_name=$(/opt/cli/get/get_device_param $device_index device_name)
 
 #define directories (2)
 APP_BUILD_DIR="/home/$username/my_projects/vitis/$project_name/build_dir.$TARGET.$platform"
@@ -195,19 +269,18 @@ fi
 cd $APP_BUILD_DIR
 xclbin=$(echo *.xclbin | awk '{print $NF}')
 
+#get BDF (i.e., Bus:Device.Function) 
+upstream_port=$(/opt/cli/get/get_device_param $device_index upstream_port)
+bdf="${upstream_port%?}1"
+
 #programming local server
 echo "Programming local server ${bold}$hostname...${normal}"
-#sgutil get serial only when we have one FPGA and not serial_found
-if [[ $(lspci | grep Xilinx | wc -l) = 1 ]] & [[ $serial_found = "0" ]]; then
-    #serial_number=$(sgutil get serial | cut -d "=" -f2)
-    serial_number="--device"
-fi
 #revert to xrt first if FPGA is already in baremetal
-sudo /opt/cli/program/revert
+sudo /opt/cli/program/revert $bdf
 #reset device (we delete any xclbin)
-/opt/xilinx/xrt/bin/xbutil reset $serial_number --force
+/opt/xilinx/xrt/bin/xbutil reset --device $bdf --force
 #program xclbin
-/opt/xilinx/xrt/bin/xbutil program $serial_number -u $xclbin
+/opt/xilinx/xrt/bin/xbutil program --device $bdf -u $xclbin
 
 #programming remote servers (if applies)
 for i in "${servers_family_list[@]}"
@@ -217,11 +290,8 @@ do
     echo "Programming remote server ${bold}$i...${normal}"
     echo ""
 
-    #we assume this for now
-    serial_number="--device"
-
     #remotely revert to xrt, reset device (we delete any xclbin) and program xclbin
-    ssh -t $username@$i "sudo /opt/cli/program/revert ; /opt/xilinx/xrt/bin/xbutil reset $serial_number --force ; /opt/xilinx/xrt/bin/xbutil program $serial_number -u $APP_BUILD_DIR/$xclbin"
+    ssh -t $username@$i "sudo /opt/cli/program/revert ; /opt/xilinx/xrt/bin/xbutil reset --device $bdf --force ; /opt/xilinx/xrt/bin/xbutil program --device $bdf -u $APP_BUILD_DIR/$xclbin"
 done
 
 echo ""
