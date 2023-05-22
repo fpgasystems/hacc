148c4c6bd83ffb29fb10dd55b1b6aee9331dcfef
Javier <javier@staff-net-vpn-dhcp-6740.intern.ethz.ch>
2023-05-22 08:37:07 +0200

    Simplified dialog verified on U55C

--- cli/program/revert.sh
+++ cli/program/revert.sh
@@ -62,44 +62,9 @@ if [ "$flags" = "" ]; then
     if [[ "$multiple_devices" == "0" ]]; then
         #servers with only one FPGA (i.e., alveo-u55c-01)
         device_index="0"
-    elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
-        #servers with four FPGAs (i.e., hacc-box-01)
-        #device_0
-        id_0=$($CLI_WORKDIR/get/get_device_param 0 id)
-        device_type_0=$($CLI_WORKDIR/get/get_device_param 0 device_type)
-        device_name_0=$($CLI_WORKDIR/get/get_device_param 0 device_name)
-        serial_number_0=$($CLI_WORKDIR/get/get_device_param 0 serial_number)
-        #device_1
-        id_1=$($CLI_WORKDIR/get/get_device_param 1 id)
-        device_type_1=$($CLI_WORKDIR/get/get_device_param 1 device_type)
-        device_name_1=$($CLI_WORKDIR/get/get_device_param 1 device_name)
-        serial_number_1=$($CLI_WORKDIR/get/get_device_param 1 serial_number)
-        #device_2
-        id_2=$($CLI_WORKDIR/get/get_device_param 2 id)
-        device_type_2=$($CLI_WORKDIR/get/get_device_param 2 device_type)
-        device_name_2=$($CLI_WORKDIR/get/get_device_param 2 device_name)
-        serial_number_2=$($CLI_WORKDIR/get/get_device_param 2 serial_number)
-        #device_3
-        id_3=$($CLI_WORKDIR/get/get_device_param 3 id)
-        device_type_3=$($CLI_WORKDIR/get/get_device_param 3 device_type)
-        device_name_3=$($CLI_WORKDIR/get/get_device_param 3 device_name)
-        serial_number_3=$($CLI_WORKDIR/get/get_device_param 3 serial_number)
-        #concatenate strings
-        devices=( "$id_0 [$device_type_0 - $device_name_0 - $serial_number_0]" "$id_1 [$device_type_1 - $device_name_1 - $serial_number_1]" "$id_2 [$device_type_2 - $device_name_2 - $serial_number_2]" "$id_3 [$device_type_3 - $device_name_3 - $serial_number_3]")
-        #multiple choice
-        echo ""
-        echo "${bold}Please, choose your device:${normal}"
-        echo ""
-        PS3=""
-        select device_index in "${devices[@]}"; do
-            if [[ -z $device_index ]]; then
-                echo "" >&/dev/null
-            else
-                device_found="1"
-                device_index=${device_index:0:1}
-                break
-            fi
-        done
+    else
+        $CLI_WORKDIR/sgutil program revert -h
+        exit
     fi
 else
     #find flags and values
@@ -117,6 +82,69 @@ else
         exit
     fi
 fi
+
+
+
+#if [ "$flags" = "" ]; then
+#    #get device index
+#    if [[ "$multiple_devices" == "0" ]]; then
+#        #servers with only one FPGA (i.e., alveo-u55c-01)
+#        device_index="0"
+#    elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
+#        #servers with four FPGAs (i.e., hacc-box-01)
+#        #device_0
+#        id_0=$($CLI_WORKDIR/get/get_device_param 0 id)
+#        device_type_0=$($CLI_WORKDIR/get/get_device_param 0 device_type)
+#        device_name_0=$($CLI_WORKDIR/get/get_device_param 0 device_name)
+#        serial_number_0=$($CLI_WORKDIR/get/get_device_param 0 serial_number)
+#        #device_1
+#        id_1=$($CLI_WORKDIR/get/get_device_param 1 id)
+#        device_type_1=$($CLI_WORKDIR/get/get_device_param 1 device_type)
+#        device_name_1=$($CLI_WORKDIR/get/get_device_param 1 device_name)
+#        serial_number_1=$($CLI_WORKDIR/get/get_device_param 1 serial_number)
+#        #device_2
+#        id_2=$($CLI_WORKDIR/get/get_device_param 2 id)
+#        device_type_2=$($CLI_WORKDIR/get/get_device_param 2 device_type)
+#        device_name_2=$($CLI_WORKDIR/get/get_device_param 2 device_name)
+#        serial_number_2=$($CLI_WORKDIR/get/get_device_param 2 serial_number)
+#        #device_3
+#        id_3=$($CLI_WORKDIR/get/get_device_param 3 id)
+#        device_type_3=$($CLI_WORKDIR/get/get_device_param 3 device_type)
+#        device_name_3=$($CLI_WORKDIR/get/get_device_param 3 device_name)
+#        serial_number_3=$($CLI_WORKDIR/get/get_device_param 3 serial_number)
+#        #concatenate strings
+#        devices=( "$id_0 [$device_type_0 - $device_name_0 - $serial_number_0]" "$id_1 [$device_type_1 - $device_name_1 - $serial_number_1]" "$id_2 [$device_type_2 - $device_name_2 - $serial_number_2]" "$id_3 [$device_type_3 - $device_name_3 - $serial_number_3]")
+#        #multiple choice
+#        echo ""
+#        echo "${bold}Please, choose your device:${normal}"
+#        echo ""
+#        PS3=""
+#        select device_index in "${devices[@]}"; do
+#            if [[ -z $device_index ]]; then
+#                echo "" >&/dev/null
+#            else
+#                device_found="1"
+#                device_index=${device_index:0:1}
+#                break
+#            fi
+#        done
+#    fi
+#else
+#    #find flags and values
+#    for (( i=0; i<${#flags[@]}; i++ ))
+#    do
+#        if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
+#            device_found="1"
+#            device_idx=$(($i+1))
+#            device_index=${flags[$device_idx]}
+#        fi    
+#    done
+#    #forbidden combinations
+#    if [[ $device_found = "0" ]] || [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 0 ))); then
+#        $CLI_WORKDIR/sgutil program revert -h
+#        exit
+#    fi
+#fi
 
 #get BDF (i.e., Bus:Device.Function) 
 upstream_port=$(/opt/cli/get/get_device_param $device_index upstream_port)
