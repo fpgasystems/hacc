c42044c0c5ff8fdf12d4aedfb8feb4ef282d064b
Javier <javier@staff-net-vpn-dhcp-6064.intern.ethz.ch>
2023-05-22 15:09:09 +0200

    Using multiple_devices function verified

--- cli/program/vitis.sh
+++ cli/program/vitis.sh
@@ -32,17 +32,7 @@ if ! [ -d "/home/$username/my_projects/vitis/" ]; then
 fi
 
 #check on multiple Xilinx devices
-num_devices=$(/opt/cli/common/get_num_devices)
-if [[ -z "$num_devices" ]] || [[ "$num_devices" -eq 0 ]]; then
-    echo ""
-    echo "Please, update $DATABASE according to your infrastructure."
-    echo ""
-    exit
-elif [[ "$num_devices" -eq 1 ]]; then
-    multiple_devices="0"
-else
-    multiple_devices="1"
-fi
+multiple_devices=$($CLI_WORKDIR/common/get_multiple_devices $DATABASE)
 
 #check on flags (before: flags cannot be empty)
 project_found="0"
