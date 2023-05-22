abe3c081183d40a702c894c413fdfb23293205b5
Javier <javier@staff-net-vpn-dhcp-6740.intern.ethz.ch>
2023-05-22 10:07:21 +0200

    MAX_DEVICES check and cleanup

--- cli/program/revert.sh
+++ cli/program/revert.sh
@@ -2,6 +2,7 @@
 
 CLI_WORKDIR="/opt/cli"
 DATABASE="/opt/hacc/devices"
+MAX_DEVICES=4
 
 bold=$(tput bold)
 normal=$(tput sgr0)
@@ -16,15 +17,6 @@ username=$USER
 # get hostname
 url="${HOSTNAME}"
 hostname="${url%%.*}"
-
-#check on BDF
-#if [ "$#" -ne 1 ] ; then
-#    echo ""
-#    echo "$0: exactly 1 argument expected. Example: /opt/cli/program/revert 81:00.1"
-#else
-#    bdf=$1 
-#    bdf="${bdf%??}" #i.e., we transform 81:00.1 into 81:00
-#fi
 
 # inputs
 read -a flags <<< "$@"
@@ -57,6 +49,7 @@ else
 fi
 
 #check on flags
+device_found="0"
 if [ "$flags" = "" ]; then
     #get device index
     if [[ "$multiple_devices" == "0" ]]; then
@@ -83,68 +76,12 @@ else
     fi
 fi
 
-
-
-#if [ "$flags" = "" ]; then
-#    #get device index
-#    if [[ "$multiple_devices" == "0" ]]; then
-#        #servers with only one FPGA (i.e., alveo-u55c-01)
-#        device_index="0"
-#    elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
-#        #servers with four FPGAs (i.e., hacc-box-01)
-#        #device_0
-#        id_0=$($CLI_WORKDIR/get/get_device_param 0 id)
-#        device_type_0=$($CLI_WORKDIR/get/get_device_param 0 device_type)
-#        device_name_0=$($CLI_WORKDIR/get/get_device_param 0 device_name)
-#        serial_number_0=$($CLI_WORKDIR/get/get_device_param 0 serial_number)
-#        #device_1
-#        id_1=$($CLI_WORKDIR/get/get_device_param 1 id)
-#        device_type_1=$($CLI_WORKDIR/get/get_device_param 1 device_type)
-#        device_name_1=$($CLI_WORKDIR/get/get_device_param 1 device_name)
-#        serial_number_1=$($CLI_WORKDIR/get/get_device_param 1 serial_number)
-#        #device_2
-#        id_2=$($CLI_WORKDIR/get/get_device_param 2 id)
-#        device_type_2=$($CLI_WORKDIR/get/get_device_param 2 device_type)
-#        device_name_2=$($CLI_WORKDIR/get/get_device_param 2 device_name)
-#        serial_number_2=$($CLI_WORKDIR/get/get_device_param 2 serial_number)
-#        #device_3
-#        id_3=$($CLI_WORKDIR/get/get_device_param 3 id)
-#        device_type_3=$($CLI_WORKDIR/get/get_device_param 3 device_type)
-#        device_name_3=$($CLI_WORKDIR/get/get_device_param 3 device_name)
-#        serial_number_3=$($CLI_WORKDIR/get/get_device_param 3 serial_number)
-#        #concatenate strings
-#        devices=( "$id_0 [$device_type_0 - $device_name_0 - $serial_number_0]" "$id_1 [$device_type_1 - $device_name_1 - $serial_number_1]" "$id_2 [$device_type_2 - $device_name_2 - $serial_number_2]" "$id_3 [$device_type_3 - $device_name_3 - $serial_number_3]")
-#        #multiple choice
-#        echo ""
-#        echo "${bold}Please, choose your device:${normal}"
-#        echo ""
-#        PS3=""
-#        select device_index in "${devices[@]}"; do
-#            if [[ -z $device_index ]]; then
-#                echo "" >&/dev/null
-#            else
-#                device_found="1"
-#                device_index=${device_index:0:1}
-#                break
-#            fi
-#        done
-#    fi
-#else
-#    #find flags and values
-#    for (( i=0; i<${#flags[@]}; i++ ))
-#    do
-#        if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
-#            device_found="1"
-#            device_idx=$(($i+1))
-#            device_index=${flags[$device_idx]}
-#        fi    
-#    done
-#    #forbidden combinations
-#    if [[ $device_found = "0" ]] || [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 0 ))); then
-#        $CLI_WORKDIR/sgutil program revert -h
-#        exit
-#    fi
-#fi
+#device_index should be between {0 .. MAX_DEVICES - 1}
+MAX_DEVICES=$(($MAX_DEVICES-1))
+if [[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 0 ]]; then
+    $CLI_WORKDIR/sgutil program revert -h
+    exit
+fi
 
 #get BDF (i.e., Bus:Device.Function) 
 upstream_port=$(/opt/cli/get/get_device_param $device_index upstream_port)
@@ -163,18 +100,6 @@ fi
 
 echo ""
 echo "${bold}sgutil program revert${normal}"
-
-#sgutil get device if there is only one FPGA and not name_found
-#if [[ $(lspci | grep Xilinx | wc -l) = 1 ]] & [[ $name_found = "0" ]]; then
-#    #device_name=$(/opt/cli/get/device | cut -d "=" -f2)
-#    device_name=$(/opt/cli/get/device -d $device_index | awk -F': ' '{print $2}' | grep -v '^$')
-#fi
-
-#sgutil get serial if there is only one FPGA and not serial_found
-#if [[ $(lspci | grep Xilinx | wc -l) = 1 ]] & [[ $serial_found = "0" ]]; then
-#    #serial_number=$(/opt/cli/get/serial | cut -d "=" -f2)
-#    serial_number=$(/opt/cli/get/serial -d $device_index | awk -F': ' '{print $2}' | grep -v '^$')
-#fi
 
 #get device and serial name
 serial_number=$(/opt/cli/get/serial -d $device_index | awk -F': ' '{print $2}' | grep -v '^$')
