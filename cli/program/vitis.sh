0fabd0cc066cef2650f48a5a3c026c994139a235
Javier <javier@staff-net-als-2082.intern.ethz.ch>
2023-05-23 11:23:50 +0200

    device_dialog

--- cli/program/vitis.sh
+++ cli/program/vitis.sh
@@ -40,7 +40,6 @@ project_name=""
 device_found=""
 device_index=""
 if [ "$flags" = "" ]; then
-    #no flags: start dialogs
     #project
     echo ""
     echo "${bold}Please, choose your $WORKFLOW project:${normal}"
@@ -48,60 +47,13 @@ if [ "$flags" = "" ]; then
     result=$($CLI_WORKDIR/common/project_dialog $username $WORKFLOW)
     project_found=$(echo "$result" | sed -n '1p')
     project_name=$(echo "$result" | sed -n '2p')
-
-    ##get device index
-    #if [[ "$multiple_devices" == "0" ]]; then
-    #    #servers with only one FPGA (i.e., alveo-u55c-01)
-    #    device_index="0"
-    #elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
-    #    #servers with four FPGAs (i.e., hacc-box-01)
-    #    #device_0
-    #    id_0=$($CLI_WORKDIR/get/get_device_param 0 id)
-    #    device_type_0=$($CLI_WORKDIR/get/get_device_param 0 device_type)
-    #    device_name_0=$($CLI_WORKDIR/get/get_device_param 0 device_name)
-    #    serial_number_0=$($CLI_WORKDIR/get/get_device_param 0 serial_number)
-    #    #device_1
-    #    id_1=$($CLI_WORKDIR/get/get_device_param 1 id)
-    #    device_type_1=$($CLI_WORKDIR/get/get_device_param 1 device_type)
-    #    device_name_1=$($CLI_WORKDIR/get/get_device_param 1 device_name)
-    #    serial_number_1=$($CLI_WORKDIR/get/get_device_param 1 serial_number)
-    #    #device_2
-    #    id_2=$($CLI_WORKDIR/get/get_device_param 2 id)
-    #    device_type_2=$($CLI_WORKDIR/get/get_device_param 2 device_type)
-    #    device_name_2=$($CLI_WORKDIR/get/get_device_param 2 device_name)
-    #    serial_number_2=$($CLI_WORKDIR/get/get_device_param 2 serial_number)
-    #    #device_3
-    #    id_3=$($CLI_WORKDIR/get/get_device_param 3 id)
-    #    device_type_3=$($CLI_WORKDIR/get/get_device_param 3 device_type)
-    #    device_name_3=$($CLI_WORKDIR/get/get_device_param 3 device_name)
-    #    serial_number_3=$($CLI_WORKDIR/get/get_device_param 3 serial_number)
-    #    #concatenate strings
-    #    devices=( "$id_0 [$device_type_0 - $device_name_0 - $serial_number_0]" "$id_1 [$device_type_1 - $device_name_1 - $serial_number_1]" "$id_2 [$device_type_2 - $device_name_2 - $serial_number_2]" "$id_3 [$device_type_3 - $device_name_3 - $serial_number_3]")
-    #    #multiple choice
-    #    echo ""
-    #    echo "${bold}Please, choose your device:${normal}"
-    #    echo ""
-    #    PS3=""
-    #    select device_index in "${devices[@]}"; do
-    #        if [[ -z $device_index ]]; then
-    #            echo "" >&/dev/null
-    #        else
-    #            device_found="1"
-    #            device_index=${device_index:0:1}
-    #            break
-    #        fi
-    #    done
-    #fi
-
     #device
     echo ""
     echo "${bold}Please, choose your device:${normal}"
     echo ""
-    result=$($CLI_WORKDIR/common/device_dialog $multiple_devices)
+    result=$($CLI_WORKDIR/common/device_dialog $CLI_WORKDIR $MAX_DEVICES $multiple_devices)
     device_found=$(echo "$result" | sed -n '1p')
     device_index=$(echo "$result" | sed -n '2p')
-
-
 else
     #find flags and values
     for (( i=0; i<${#flags[@]}; i++ ))
