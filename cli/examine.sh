bb898da055cb42266f8ea3c03d4454d84fc9dca1
Javier <javier@staff-net-als-2082.intern.ethz.ch>
2023-05-23 14:34:48 +0200

    Moved bdf calculation

--- cli/examine.sh
+++ cli/examine.sh
@@ -78,7 +78,6 @@ fi
 #device_1
 id_1=$(/opt/cli/get/get_device_param 1 id)
 upstream_port_1=$(/opt/cli/get/get_device_param 1 upstream_port)
-bdf_1="${upstream_port_1::-1}1"
 device_type_1=$(/opt/cli/get/get_device_param 1 device_type)
 device_name_1=$(/opt/cli/get/get_device_param 1 device_name)
 serial_number_1=$(/opt/cli/get/get_device_param 1 serial_number)
@@ -88,7 +87,6 @@ mac_1=$(/opt/cli/get/get_device_param 1 MAC)
 #device_2
 id_2=$(/opt/cli/get/get_device_param 2 id)
 upstream_port_2=$(/opt/cli/get/get_device_param 2 upstream_port)
-bdf_2="${upstream_port_2::-1}1"
 device_type_2=$(/opt/cli/get/get_device_param 2 device_type)
 device_name_2=$(/opt/cli/get/get_device_param 2 device_name)
 serial_number_2=$(/opt/cli/get/get_device_param 2 serial_number)
@@ -98,7 +96,6 @@ mac_2=$(/opt/cli/get/get_device_param 2 MAC)
 #device_3
 id_3=$(/opt/cli/get/get_device_param 3 id)
 upstream_port_3=$(/opt/cli/get/get_device_param 3 upstream_port)
-bdf_3="${upstream_port_3::-1}1"
 device_type_3=$(/opt/cli/get/get_device_param 3 device_type)
 device_name_3=$(/opt/cli/get/get_device_param 3 device_name)
 serial_number_3=$(/opt/cli/get/get_device_param 3 serial_number)
@@ -108,7 +105,6 @@ mac_3=$(/opt/cli/get/get_device_param 3 MAC)
 #device_4
 id_4=$(/opt/cli/get/get_device_param 4 id)
 upstream_port_4=$(/opt/cli/get/get_device_param 4 upstream_port)
-bdf_4="${upstream_port_4::-1}1"
 device_type_4=$(/opt/cli/get/get_device_param 4 device_type)
 device_name_4=$(/opt/cli/get/get_device_param 4 device_name)
 serial_number_4=$(/opt/cli/get/get_device_param 4 serial_number)
@@ -120,6 +116,8 @@ mac_4=$(/opt/cli/get/get_device_param 4 MAC)
 
 #device_1
 if [ -n "$id_1" ]; then  
+  #get bdf
+  bdf_1="${upstream_port_1::-1}1"
   #adjust length
   aux="$device_type_1 ($device_name_1)"
   diff=$(( $STR_LENGTH - ${#aux} ))
@@ -135,6 +133,8 @@ fi
 
 #device_2
 if [ -n "$id_2" ]; then
+  #get bdf
+  bdf_2="${upstream_port_2::-1}1"
   #adjust length
   aux="$device_type_1 ($device_name_1)"
   diff=$(( $STR_LENGTH - ${#aux} ))
@@ -149,6 +149,8 @@ fi
 
 #device_3 (acap)
 if [ -n "$id_3" ]; then
+  #get bdf
+  bdf_3="${upstream_port_3::-1}1"
   #adjust length
   aux="$device_type_1 ($device_name_1)"
   diff=$(( $STR_LENGTH - ${#aux} ))
@@ -163,6 +165,8 @@ fi
 
 #device_4 (acap)
 if [ -n "$id_4" ]; then
+  #get bdf
+  bdf_4="${upstream_port_4::-1}1"
   #adjust length
   aux="$device_type_1 ($device_name_1)"
   diff=$(( $STR_LENGTH - ${#aux} ))
