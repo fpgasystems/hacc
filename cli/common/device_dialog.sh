5bd30f0144115f8b0ac062dad9018aaa09a836b1
Javier <javier@staff-net-als-2082.intern.ethz.ch>
2023-05-23 08:45:24 +0200

    Cleanup

--- cli/common/device_dialog.sh
+++ cli/common/device_dialog.sh
@@ -12,48 +12,20 @@ declare -g device_index=""
 if [[ "$multiple_devices" == "0" ]]; then
     #servers with only one FPGA (i.e., alveo-u55c-01)
     device_index="0"
-elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
+elif [[ "$multiple_devices" == "1" ]]; then 
     #servers with four FPGAs (i.e., hacc-box-01)
-    ##device_0
-    #id_0=$($CLI_WORKDIR/get/get_device_param 0 id)
-    #device_type_0=$($CLI_WORKDIR/get/get_device_param 0 device_type)
-    #device_name_0=$($CLI_WORKDIR/get/get_device_param 0 device_name)
-    #serial_number_0=$($CLI_WORKDIR/get/get_device_param 0 serial_number)
-    ##device_1
-    #id_1=$($CLI_WORKDIR/get/get_device_param 1 id)
-    #device_type_1=$($CLI_WORKDIR/get/get_device_param 1 device_type)
-    #device_name_1=$($CLI_WORKDIR/get/get_device_param 1 device_name)
-    #serial_number_1=$($CLI_WORKDIR/get/get_device_param 1 serial_number)
-    ##device_2
-    #id_2=$($CLI_WORKDIR/get/get_device_param 2 id)
-    #device_type_2=$($CLI_WORKDIR/get/get_device_param 2 device_type)
-    #device_name_2=$($CLI_WORKDIR/get/get_device_param 2 device_name)
-    #serial_number_2=$($CLI_WORKDIR/get/get_device_param 2 serial_number)
-    ##device_3
-    #id_3=$($CLI_WORKDIR/get/get_device_param 3 id)
-    #device_type_3=$($CLI_WORKDIR/get/get_device_param 3 device_type)
-    #device_name_3=$($CLI_WORKDIR/get/get_device_param 3 device_name)
-    #serial_number_3=$($CLI_WORKDIR/get/get_device_param 3 serial_number)
-    ##concatenate strings
-    #devices=( "$id_0 [$device_type_0 - $device_name_0 - $serial_number_0]" "$id_1 [$device_type_1 - $device_name_1 - $serial_number_1]" "$id_2 [$device_type_2 - $device_name_2 - $serial_number_2]" "$id_3 [$device_type_3 - $device_name_3 - $serial_number_3]")
-    
-    
-    # Declare an empty array to store the device strings
+    #declare an empty array to store the device strings
     devices=()
-
-    # Iterate over the indices 0 to MAX_DEVICES-1 using a for loop
+    #iterate over the indices 0 to MAX_DEVICES-1 using a for loop
     for ((i=0; i<MAX_DEVICES; i++)); do
-        # Retrieve the parameters for each device using the current index
+        #retrieve the parameters for each device using the current index
         id=$($CLI_WORKDIR/get/get_device_param $i id)
         device_type=$($CLI_WORKDIR/get/get_device_param $i device_type)
         device_name=$($CLI_WORKDIR/get/get_device_param $i device_name)
         serial_number=$($CLI_WORKDIR/get/get_device_param $i serial_number)
-        
-        # Concatenate the parameter values into a single string and add it to the array
+        #concatenate the parameter values into a single string and add it to the array
         devices+=("$id [$device_type - $device_name - $serial_number]")
     done
-    
-    
     #multiple choice
     #echo ""
     #echo "${bold}Please, choose your device:${normal}"
@@ -70,6 +42,6 @@ elif [[ "$multiple_devices" == "1" ]]; then #$(lspci | grep Xilinx | wc -l) = 8
     done
 fi
 
-# Return the values of device_found and device_index
+#return the values of device_found and device_index
 echo "$device_found"
 echo "$device_index"
