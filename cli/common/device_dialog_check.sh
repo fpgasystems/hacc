1e40c078c388365e17dd0b64036ab798a155e04c
Javier <javier@staff-net-vpn-dhcp-6405.intern.ethz.ch>
2023-05-24 10:11:40 +0200

    Current working versions

--- cli/common/device_dialog_check.sh
+++ cli/common/device_dialog_check.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 flags=("$@")  # Assign command-line arguments to the 'flags' array
+#CLI_WORKDIR="$1"                 # Assign the first argument to the variable cli_workdir
+#shift                            # Shift the remaining arguments by one position
+#flags=("$@")                     # Assign the shifted arguments to the flags array
+#multiple_devices="${flags[-1]}"  # Retrieve the last element as the value for multiple_devices
 
 # Declare global variables
-declare -g device_found=""
+declare -g device_found="0"
 declare -g device_idx=""
 declare -g device_index=""
-declare -g device_error=""
+#declare -g device_error=""
 
+#read flags
 for (( i=0; i<${#flags[@]}; i++ ))
 do
     if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
@@ -17,14 +22,15 @@ do
     fi  
 done
 
-if [[ $device_found = "0" ]] || [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 0 ))); then
-    #$CLI_WORKDIR/sgutil program vitis -h
-    #exit
-    device_error="1"
-fi
+#forbidden combinations
+#if [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 0 ))); then #[[ $device_found = "0" ]] || 
+#    $CLI_WORKDIR/sgutil program vitis -h
+#    exit
+#    #device_error="1"
+#fi
 
 #return the values
 echo "$device_found"
 echo "$device_idx"
 echo "$device_index"
-echo "$device_error"
+#echo "$device_error"
--- cli/common/project_dialog_check.sh
+++ cli/common/project_dialog_check.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 flags=("$@")  # Assign command-line arguments to the 'flags' array
+#CLI_WORKDIR=$1        # Assign the first argument to the variable cli_workdir
+#shift                 # Shift the remaining arguments by one position
+#flags=("$@")          # Assign the shifted arguments to the flags array
 
 # Declare global variables
-declare -g project_found=""
+declare -g project_found="0"
 declare -g project_idx=""
 declare -g project_name=""
-declare -g project_error=""
+#declare -g project_error=""
 
+#read flags
 for (( i=0; i<${#flags[@]}; i++ ))
 do
     if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --project " ]]; then # flags[i] is -p or --project
@@ -17,14 +21,15 @@ do
     fi
 done
 
-if [[ $project_found = "0" ]] || ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $device_found = "1" ]) || ([ "$device_found" = "1" ] && [ "$device_index" = "" ]); then
-    #$CLI_WORKDIR/sgutil program vitis -h
-    #exit
-    project_error="1"
-fi
+##forbidden combinations
+#if ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $device_found = "1" ]) || ([ "$device_found" = "1" ] && [ "$device_index" = "" ]); then #[[ $project_found = "0" ]] || 
+#    $CLI_WORKDIR/sgutil program vitis -h
+#    exit
+#    #project_error="1"
+#fi
 
 #return the values
 echo "$project_found"
 echo "$project_idx"
 echo "$project_name"
-echo "$project_error"
+#echo "$project_error"
