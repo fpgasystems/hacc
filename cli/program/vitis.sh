5728aeeb6328f252ce83c5a7b0c8c3982c655649
Javier <javier@staff-net-vpn-dhcp-6563.intern.ethz.ch>
2023-05-25 14:52:15 +0200

    remove comment

--- cli/program/vitis.sh
+++ cli/program/vitis.sh
@@ -76,7 +76,7 @@ else
     device_found=$(echo "$result" | sed -n '1p')
     device_index=$(echo "$result" | sed -n '2p')
     #forbidden combinations
-    if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then #[[ $device_found = "0" ]] || 
+    if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then
         $CLI_WORKDIR/sgutil program vitis -h
         exit
     fi
