07d569173b031649c68d7b24c7f8e0d3197bbaf4
Javier <javier@staff-net-als-2082.intern.ethz.ch>
2023-05-26 11:57:52 +0200

    Loop version verified

--- cli/examine.sh
+++ cli/examine.sh
@@ -68,103 +68,64 @@ if [[ $upstream_port_1 == "xx:xx.x" || $root_port_0 == "xx:xx.x" || $LinkCtl_0 =
 fi
 
 #device_1
-id_1=$(/opt/cli/get/get_device_param 1 id)
-upstream_port_1=$(/opt/cli/get/get_device_param 1 upstream_port)
-device_type_1=$(/opt/cli/get/get_device_param 1 device_type)
-device_name_1=$(/opt/cli/get/get_device_param 1 device_name)
-serial_number_1=$(/opt/cli/get/get_device_param 1 serial_number)
-ip_1=$(/opt/cli/get/get_device_param 1 IP)
-mac_1=$(/opt/cli/get/get_device_param 1 MAC)
+#id_1=$(/opt/cli/get/get_device_param 1 id)
+#upstream_port_1=$(/opt/cli/get/get_device_param 1 upstream_port)
+#device_type_1=$(/opt/cli/get/get_device_param 1 device_type)
+#device_name_1=$(/opt/cli/get/get_device_param 1 device_name)
+#serial_number_1=$(/opt/cli/get/get_device_param 1 serial_number)
+#ip_1=$(/opt/cli/get/get_device_param 1 IP)
+#mac_1=$(/opt/cli/get/get_device_param 1 MAC)
 
 #device_2
-id_2=$(/opt/cli/get/get_device_param 2 id)
-upstream_port_2=$(/opt/cli/get/get_device_param 2 upstream_port)
-device_type_2=$(/opt/cli/get/get_device_param 2 device_type)
-device_name_2=$(/opt/cli/get/get_device_param 2 device_name)
-serial_number_2=$(/opt/cli/get/get_device_param 2 serial_number)
-ip_2=$(/opt/cli/get/get_device_param 2 IP)
-mac_2=$(/opt/cli/get/get_device_param 2 MAC)
+#id_2=$(/opt/cli/get/get_device_param 2 id)
+#upstream_port_2=$(/opt/cli/get/get_device_param 2 upstream_port)
+#device_type_2=$(/opt/cli/get/get_device_param 2 device_type)
+#device_name_2=$(/opt/cli/get/get_device_param 2 device_name)
+#serial_number_2=$(/opt/cli/get/get_device_param 2 serial_number)
+#ip_2=$(/opt/cli/get/get_device_param 2 IP)
+#mac_2=$(/opt/cli/get/get_device_param 2 MAC)
 
 #device_3
-id_3=$(/opt/cli/get/get_device_param 3 id)
-upstream_port_3=$(/opt/cli/get/get_device_param 3 upstream_port)
-device_type_3=$(/opt/cli/get/get_device_param 3 device_type)
-device_name_3=$(/opt/cli/get/get_device_param 3 device_name)
-serial_number_3=$(/opt/cli/get/get_device_param 3 serial_number)
-ip_3=$(/opt/cli/get/get_device_param 3 IP)
-mac_3=$(/opt/cli/get/get_device_param 3 MAC)
+#id_3=$(/opt/cli/get/get_device_param 3 id)
+#upstream_port_3=$(/opt/cli/get/get_device_param 3 upstream_port)
+#device_type_3=$(/opt/cli/get/get_device_param 3 device_type)
+#device_name_3=$(/opt/cli/get/get_device_param 3 device_name)
+#serial_number_3=$(/opt/cli/get/get_device_param 3 serial_number)
+#ip_3=$(/opt/cli/get/get_device_param 3 IP)
+#mac_3=$(/opt/cli/get/get_device_param 3 MAC)
 
 #device_4
-id_4=$(/opt/cli/get/get_device_param 4 id)
-upstream_port_4=$(/opt/cli/get/get_device_param 4 upstream_port)
-device_type_4=$(/opt/cli/get/get_device_param 4 device_type)
-device_name_4=$(/opt/cli/get/get_device_param 4 device_name)
-serial_number_4=$(/opt/cli/get/get_device_param 4 serial_number)
-ip_4=$(/opt/cli/get/get_device_param 4 IP)
-mac_4=$(/opt/cli/get/get_device_param 4 MAC)
+#id_4=$(/opt/cli/get/get_device_param 4 id)
+#upstream_port_4=$(/opt/cli/get/get_device_param 4 upstream_port)
+#device_type_4=$(/opt/cli/get/get_device_param 4 device_type)
+#device_name_4=$(/opt/cli/get/get_device_param 4 device_name)
+#serial_number_4=$(/opt/cli/get/get_device_param 4 serial_number)
+#ip_4=$(/opt/cli/get/get_device_param 4 IP)
+#mac_4=$(/opt/cli/get/get_device_param 4 MAC)
 
-#device_1
-if [ -n "$id_1" ]; then  
-  #get bdf
-  bdf_1="${upstream_port_1::-1}1"
-  #adjust length
-  aux="$device_type_1 ($device_name_1)"
-  diff=$(( $STR_LENGTH - ${#aux} ))
-  aux="$aux$(printf '%*s' $diff)"
-  #split ip
-  add_0=$(split_addresses $ip_1 $mac_1 0)
-  add_1=$(split_addresses $ip_1 $mac_1 1)
-  #print
-  echo "$id_1            : $upstream_port_1 ($bdf_1)   : $aux : $serial_number_1 : $add_0"
-  echo "                                                                            $add_1"
-fi
-
-#device_2
-if [ -n "$id_2" ]; then
-  #get bdf
-  bdf_2="${upstream_port_2::-1}1"
-  #adjust length
-  aux="$device_type_1 ($device_name_1)"
-  diff=$(( $STR_LENGTH - ${#aux} ))
-  aux="$aux$(printf '%*s' $diff)"
-  #split ip
-  add_0=$(split_addresses $ip_2 $mac_2 0)
-  add_1=$(split_addresses $ip_2 $mac_2 1)
-  #print
-  echo "$id_2            : $upstream_port_2 ($bdf_2)   : $device_type_2 ($device_name_2) : $serial_number_2 : $add_0"
-  echo "                                                                            $add_1"
-fi
-
-#device_3 (acap)
-if [ -n "$id_3" ]; then
-  #get bdf
-  bdf_3="${upstream_port_3::-1}1"
-  #adjust length
-  aux="$device_type_1 ($device_name_1)"
-  diff=$(( $STR_LENGTH - ${#aux} ))
-  aux="$aux$(printf '%*s' $diff)"
-  #split ip
-  add_0=$(split_addresses $ip_3 $mac_3 0)
-  add_1=$(split_addresses $ip_3 $mac_3 1)
-  #print
-  echo "$id_3            : $upstream_port_3 ($bdf_3)   : $device_type_3 ($device_name_3)    : $serial_number_3 : $add_0"
-  echo "                                                                            $add_1"
-fi
-
-#device_4 (acap)
-if [ -n "$id_4" ]; then
-  #get bdf
-  bdf_4="${upstream_port_4::-1}1"
-  #adjust length
-  aux="$device_type_1 ($device_name_1)"
-  diff=$(( $STR_LENGTH - ${#aux} ))
-  aux="$aux$(printf '%*s' $diff)"
-  #split ip
-  add_0=$(split_addresses $ip_4 $mac_4 0)
-  add_1=$(split_addresses $ip_4 $mac_4 1)
-  #print
-  echo "$id_4            : $upstream_port_4 ($bdf_4)   : $device_type_4 ($device_name_4)    : $serial_number_4 : $add_0"
-  echo "                                                                            $add_1"
-fi
+for ((i=1; i<=$MAX_RECONF_DEVICES; i++)); do
+  id=$(/opt/cli/get/get_device_param $i id)
+  upstream_port=$(/opt/cli/get/get_device_param $i upstream_port)
+  device_type=$(/opt/cli/get/get_device_param $i device_type)
+  device_name=$(/opt/cli/get/get_device_param $i device_name)
+  serial_number=$(/opt/cli/get/get_device_param $i serial_number)
+  ip=$(/opt/cli/get/get_device_param $i IP)
+  mac=$(/opt/cli/get/get_device_param $i MAC)
+  #print table
+  if [ -n "$id" ]; then  
+    #get bdf
+    bdf="${upstream_port::-1}1"
+    #adjust length
+    aux="$device_type ($device_name)"
+    diff=$(( $STR_LENGTH - ${#aux} ))
+    aux="$aux$(printf '%*s' $diff)"
+    #split ip
+    add_0=$(split_addresses $ip $mac 0)
+    add_1=$(split_addresses $ip $mac 1)
+    #print row
+    echo "$id            : $upstream_port ($bdf)   : $aux : $serial_number : $add_0"
+    echo "                                                                            $add_1"
+  fi
+done
 
 echo ""
