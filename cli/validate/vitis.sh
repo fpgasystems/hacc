0647fe76a19910db76c4d4ada83928ad67c1044a
Javier <javier@staff-net-vpn-dhcp-6833.intern.ethz.ch>
2023-05-26 09:03:28 +0200

    new paths and get rid of get_MAX_DEVICES

--- cli/validate/vitis.sh
+++ cli/validate/vitis.sh
@@ -3,14 +3,13 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-
-
 #constants
-CLI_WORKDIR="/opt/cli"
-DATABASE="/opt/hacc/devices"
+CLI_PATH="/opt/cli"
+HACC_PATH="/opt/hacc"
+DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
 
-#configuration parameters
-MAX_DEVICES=$($CLI_WORKDIR/common/get_MAX_DEVICES)
+#get number of fpga and acap devices present
+MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
 
 #get username
 username=$USER
@@ -19,7 +18,7 @@ username=$USER
 read -a flags <<< "$@"
 
 #check on multiple Xilinx devices
-multiple_devices=$($CLI_WORKDIR/common/get_multiple_devices $DATABASE)
+multiple_devices=$($CLI_PATH/common/get_multiple_devices $DEVICES_LIST)
 
 #check on flags
 device_found=""
@@ -36,18 +35,18 @@ if [ "$flags" = "" ]; then
         echo ""
         echo "${bold}Please, choose your device:${normal}"
         echo ""
-        result=$($CLI_WORKDIR/common/device_dialog $CLI_WORKDIR $MAX_DEVICES $multiple_devices)
+        result=$($CLI_PATH/common/device_dialog $CLI_PATH $MAX_DEVICES $multiple_devices)
         device_found=$(echo "$result" | sed -n '1p')
         device_index=$(echo "$result" | sed -n '2p')
     fi
 else
     #device_dialog_check
-    result="$("$CLI_WORKDIR/common/device_dialog_check" "${flags[@]}")"
+    result="$("$CLI_PATH/common/device_dialog_check" "${flags[@]}")"
     device_found=$(echo "$result" | sed -n '1p')
     device_index=$(echo "$result" | sed -n '2p')
     #forbidden combinations
     if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then
-        $CLI_WORKDIR/sgutil validate vitis -h
+        $CLI_PATH/sgutil validate vitis -h
         exit
     fi
     #header (2/2)
@@ -58,7 +57,7 @@ else
         echo ""
         echo "${bold}Please, choose your device:${normal}"
         echo ""
-        result=$($CLI_WORKDIR/common/device_dialog $CLI_WORKDIR $MAX_DEVICES $multiple_devices)
+        result=$($CLI_PATH/common/device_dialog $CLI_PATH $MAX_DEVICES $multiple_devices)
         device_found=$(echo "$result" | sed -n '1p')
         device_index=$(echo "$result" | sed -n '2p')
     fi
@@ -67,7 +66,7 @@ fi
 echo ""
 
 #get BDF from device_idx
-upstream_port=$($CLI_WORKDIR/get/get_device_param $device_index upstream_port)
+upstream_port=$($CLI_PATH/get/get_device_param $device_index upstream_port)
 bdf=$(echo "$upstream_port" | sed 's/0$/1/')
 
 #validate
