1fe7f93213701cdfe0719a9d8f28c4af664ceae4
Javier <javier@staff-net-vpn-dhcp-6940.intern.ethz.ch>
2023-05-30 18:46:27 +0200

    Reviewed version

--- cli/get/device.sh
+++ cli/get/device.sh
@@ -1,145 +1,54 @@
 #!/bin/bash
 
-DATABASE="/opt/hacc/devices"
-
 bold=$(tput bold)
 normal=$(tput sgr0)
 
 #constants
-CLI_WORKDIR="/opt/cli"
-MAX_DEVICES=4
+CLI_PATH="/opt/cli"
+HACC_PATH="/opt/hacc"
+DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
 
-#get hostname
-#url="${HOSTNAME}"
-#hostname="${url%%.*}"
-
-split_addresses (){
-
-  str_ip=$1
-  str_mac=$2
-  aux=$3
-
-  # Save the current IFS
-  OLDIFS=$IFS
-
-  # Set the IFS to / to split the string at each /
-  IFS="/"
-
-  # Read the two parts of the string into variables
-  read ip0 ip1 <<< "$str_ip"
-  read mac0 mac1 <<< "$str_mac"
-
-  # Reset the IFS to its original value
-  IFS=$OLDIFS
-
-  # Print the two parts of the string
-  if [[ "$aux" == "0" ]]; then
-    echo "$ip0 ($mac0)"
-  else
-    echo "$ip1 ($mac1)"
-  fi
-
-}
+#get number of fpga and acap devices present
+MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
 
 #inputs
 read -a flags <<< "$@"
 
-# Check if the DATABASE exists
-if [[ ! -f "$DATABASE" ]]; then
-  echo ""
-  echo "Please, update $DATABASE according to your infrastructure."
-  echo ""
-  exit 1
-fi
-
-#the file exists - check its contents by evaluating first row (device_0)
-device_0=$(head -n 1 "$DATABASE")
-
-#extract the second, third, and fourth columns (upstream_port, root_port, LinkCtl) using awk
-upstream_port_0=$(echo "$device_0" | awk '{print $2}')
-root_port_0=$(echo "$device_0" | awk '{print $3}')
-LinkCtl_0=$(echo "$device_0" | awk '{print $4}')
-
-#check on non-edited contents
-if [[ $upstream_port_0 == "xx:xx.x" || $root_port_0 == "xx:xx.x" || $LinkCtl_0 == "xx" ]]; then
-  echo ""
-  echo "Please, update $DATABASE according to your infrastructure."
-  echo ""
-  exit
-fi
-
 #check on multiple Xilinx devices
-multiple_devices=""
-devices=$(wc -l < $DATABASE)
-if [ -s $DATABASE ]; then
-    if [ "$devices" -eq 1 ]; then
-        multiple_devices="0"
-    else
-        multiple_devices="1"
-    fi
-else
-    echo ""
-    echo "Please, update $DATABASE according to your infrastructure."
-    echo ""
-    exit
-fi
-
-#if [[ -z $(lspci | grep Xilinx) ]]; then
-#    multiple_devices=""
-#    echo "No Xilinx device found."
-#    echo ""
-#    exit
-#elif [[ $(lspci | grep Xilinx | wc -l) = 2 ]]; then
-#    #servers with only one FPGA (i.e., alveo-u55c-01)
-#    multiple_devices="0"
-#elif [[ $(lspci | grep Xilinx | wc -l) -gt 2 ]]; then
-#    #servers with eight FPGAs (i.e., alveo-u280)
-#    multiple_devices="1"
-#else
-#    echo "Unexpected number of Xilinx devices."
-#    echo ""
-#    exit
-#fi
-
-echo ""
+multiple_devices=$($CLI_PATH/common/get_multiple_devices $DEVICES_LIST)
 
 #check on flags
 device_found=""
 device_index=""
 if [ "$flags" = "" ]; then
     #print devices information
-    for device_index in 1 2 3 4; do #0 1 2 3
-        name=$($CLI_WORKDIR/get/get_fpga_device_param $device_index device_name)
+    echo ""
+    for device_index in $(seq 1 $MAX_DEVICES); do 
+        name=$($CLI_PATH/get/get_fpga_device_param $device_index device_name)
         if [ -n "$name" ]; then
-            #type=$($CLI_WORKDIR/get/get_fpga_device_param $device_index device_type)
-            echo "$device_index: $name" #"$device_index: $type - $name"
+            echo "$device_index: $name"
         fi
     done
     echo ""
 else
-    #find flags and values
-    for (( i=0; i<${#flags[@]}; i++ ))
-    do
-        if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
-            device_found="1"
-            device_idx=$(($i+1))
-            device_index=${flags[$device_idx]}
-        fi  
-    done
+    #device_dialog_check
+    result="$("$CLI_PATH/common/device_dialog_check" "${flags[@]}")"
+    device_found=$(echo "$result" | sed -n '1p')
+    device_index=$(echo "$result" | sed -n '2p')
     #forbidden combinations
-    if [[ $device_found = "0" ]] || [[ $device_index = "" ]] || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))); then
-        $CLI_WORKDIR/sgutil get device -h
+    if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then
+        $CLI_PATH/sgutil get device -h
         exit
     fi
-    #device_index should be between {0 .. MAX_DEVICES - 1}
-    #MAX_DEVICES=$(($MAX_DEVICES-1))
-    if [[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]]; then
-        $CLI_WORKDIR/sgutil get device -h
+    #device_dialog (forgotten mandatory 2)
+    if [[ $multiple_devices = "0" ]]; then
+        device_found="1"
+        device_index="1"
+    elif [[ $device_found = "0" ]]; then
+        $CLI_PATH/sgutil get device -h
         exit
     fi
     #print
-    name=$($CLI_WORKDIR/get/get_fpga_device_param $device_index device_name)
-    #type=$($CLI_WORKDIR/get/get_fpga_device_param $device_index device_type)
-    echo "$device_index: $name" #"$device_index: $type - $name"
-    echo ""
+    name=$($CLI_PATH/get/get_fpga_device_param $device_index device_name)
+    echo "$device_index: $name"
 fi
