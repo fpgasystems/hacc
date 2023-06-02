6ea6c0f86fcaf37b072f90d289f2978f710ca982
Javier <javier@staff-net-vpn-dhcp-6952.intern.ethz.ch>
2023-06-02 07:28:47 +0200

    devices_list_check add

---
+++ cli/common/device_list_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEVICES_LIST=$1
+
+if [[ ! -f "$DEVICES_LIST" ]]; then
+  exit
+else
+  # Print if the first fpga/acap is valid
+  device_1=$(head -n 1 "$DEVICES_LIST")
+  upstream_port_1=$(echo "$device_1" | awk '{print $2}') 
+  if ! lspci | grep -qz $upstream_port_1; then
+    exit
+  fi
+fi
