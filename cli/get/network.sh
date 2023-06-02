25dca01407c577c404fb1d9e7d7a218c939fd1a8
Javier <javier@staff-net-vpn-dhcp-6952.intern.ethz.ch>
2023-06-02 07:46:03 +0200

    Using script with source

--- cli/get/network.sh
+++ cli/get/network.sh
@@ -37,16 +37,18 @@ split_addresses (){
 read -a flags <<< "$@"
 
 #check on DEVICES_LIST
-if [[ ! -f "$DEVICES_LIST" ]]; then
-  exit
-else
-  # Print if the first fpga/acap is valid
-  device_1=$(head -n 1 "$DEVICES_LIST")
-  upstream_port_1=$(echo "$device_1" | awk '{print $2}') 
-  if ! lspci | grep -qz $upstream_port_1; then
-    exit
-  fi
-fi
+source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
+
+#if [[ ! -f "$DEVICES_LIST" ]]; then
+#  exit
+#else
+#  # Print if the first fpga/acap is valid
+#  device_1=$(head -n 1 "$DEVICES_LIST")
+#  upstream_port_1=$(echo "$device_1" | awk '{print $2}') 
+#  if ! lspci | grep -qz $upstream_port_1; then
+#    exit
+#  fi
+#fi
 
 #check on multiple Xilinx devices
 multiple_devices=$($CLI_PATH/common/get_multiple_devices $DEVICES_LIST)
