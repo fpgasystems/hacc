94909d0c52fb6013e49497e77beb0429e4c2bfe4
Javier <javier@staff-net-vpn-dhcp-7078.intern.ethz.ch>
2023-06-05 12:42:54 +0200

    Cleanup

--- cli/get/serial.sh
+++ cli/get/serial.sh
@@ -17,68 +17,8 @@ MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
 #inputs
 read -a flags <<< "$@"
 
-#helper functions
-split_addresses (){
-  #input parameters
-  str_ip=$1
-  str_mac=$2
-  aux=$3
-  #save the current IFS
-  OLDIFS=$IFS
-  #set the IFS to / to split the string at each /
-  IFS="/"
-  #read the two parts of the string into variables
-  read ip0 ip1 <<< "$str_ip"
-  read mac0 mac1 <<< "$str_mac"
-  #reset the IFS to its original value
-  IFS=$OLDIFS
-  #print the two parts of the string
-  if [[ "$aux" == "0" ]]; then
-    echo "$ip0 ($mac0)"
-  else
-    echo "$ip1 ($mac1)"
-  fi
-}
-
-# Check if the DEVICES_LIST exists
-if [[ ! -f "$DEVICES_LIST" ]]; then
-  echo ""
-  echo "Please, update $DEVICES_LIST according to your infrastructure."
-  echo ""
-  exit 1
-fi
-
-#the file exists - check its contents by evaluating first row (device_0)
-device_0=$(head -n 1 "$DEVICES_LIST")
-
-#extract the second, third, and fourth columns (upstream_port, root_port, LinkCtl) using awk
-upstream_port_0=$(echo "$device_0" | awk '{print $2}')
-root_port_0=$(echo "$device_0" | awk '{print $3}')
-LinkCtl_0=$(echo "$device_0" | awk '{print $4}')
-
-#check on non-edited contents
-if [[ $upstream_port_0 == "xx:xx.x" || $root_port_0 == "xx:xx.x" || $LinkCtl_0 == "xx" ]]; then
-  echo ""
-  echo "Please, update $DEVICES_LIST according to your infrastructure."
-  echo ""
-  exit
-fi
-
 #check on multiple Xilinx devices
-multiple_devices=""
-devices=$(wc -l < $DEVICES_LIST)
-if [ -s $DEVICES_LIST ]; then
-    if [ "$devices" -eq 1 ]; then
-        multiple_devices="0"
-    else
-        multiple_devices="1"
-    fi
-else
-    echo ""
-    echo "Please, update $DEVICES_LIST according to your infrastructure."
-    echo ""
-    exit
-fi
+multiple_devices=$($CLI_PATH/common/get_multiple_devices $DEVICES_LIST)
 
 echo ""
 
