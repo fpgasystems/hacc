2e52cd3504881646a265d954cb52e61d3dc4560e
Javier <javier@staff-net-vpn-dhcp-7065.intern.ethz.ch>
2023-06-05 05:35:04 +0200

    Inverting the conditions order makes the trick

--- cli/common/device_list_check.sh
+++ cli/common/device_list_check.sh
@@ -2,13 +2,14 @@
 
 DEVICES_LIST=$1
 
-if [[ ! -f "$DEVICES_LIST" ]]; then
-  exit
-else
+if [[ -f "$DEVICES_LIST" ]]; then
   # Print if the first fpga/acap is valid
   device_1=$(head -n 1 "$DEVICES_LIST")
   upstream_port_1=$(echo "$device_1" | awk '{print $2}') 
   if ! lspci | grep -qz $upstream_port_1; then
     exit
   fi
+else
+  #DEVICES_LIST does not exist
+  exit
 fi
