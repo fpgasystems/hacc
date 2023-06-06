6cd81754b4624a1e3a5adde3b74f3232fcc80c18
Javier <javier@staff-net-als-2082.intern.ethz.ch>
2023-06-06 08:55:44 +0200

    Cleanup verified

--- cli/validate/vitis.sh
+++ cli/validate/vitis.sh
@@ -6,19 +6,23 @@ normal=$(tput sgr0)
 #constants
 CLI_PATH="/opt/cli"
 HACC_PATH="/opt/hacc"
+XRT_PATH="/opt/xilinx/xrt"
 DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
-
-#get number of fpga and acap devices present
-MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
 
 #get username
 username=$USER
 
+#check on DEVICES_LIST
+source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
+
+#get number of fpga and acap devices present
+MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
+
+#check on multiple devices
+multiple_devices=$($CLI_PATH/common/get_multiple_devices $MAX_DEVICES)
+
 #inputs
 read -a flags <<< "$@"
-
-#check on multiple Xilinx devices
-multiple_devices=$($CLI_PATH/common/get_multiple_devices $DEVICES_LIST)
 
 #check on flags
 device_found=""
@@ -39,6 +43,12 @@ if [ "$flags" = "" ]; then
         device_found=$(echo "$result" | sed -n '1p')
         device_index=$(echo "$result" | sed -n '2p')
     fi
+    #validate
+    echo ""
+    upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
+    bdf=$(echo "$upstream_port" | sed 's/0$/1/')
+    $XRT_PATH/bin/xbutil validate --device $bdf
+    echo "" 
 else
     #device_dialog_check
     result="$("$CLI_PATH/common/device_dialog_check" "${flags[@]}")"
@@ -57,22 +67,13 @@ else
         device_found="1"
         device_index="1"
     elif [[ $device_found = "0" ]]; then
-        echo ""
-        echo "${bold}Please, choose your device:${normal}"
-        echo ""
-        result=$($CLI_PATH/common/device_dialog $CLI_PATH $MAX_DEVICES $multiple_devices)
-        device_found=$(echo "$result" | sed -n '1p')
-        device_index=$(echo "$result" | sed -n '2p')
+        $CLI_PATH/sgutil validate vitis -h
+        exit
     fi
+    #validate
+    echo ""
+    upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
+    bdf=$(echo "$upstream_port" | sed 's/0$/1/')
+    $XRT_PATH/bin/xbutil validate --device $bdf
+    echo ""    
 fi
-
-echo ""
-
-#get BDF from device_idx
-upstream_port=$($CLI_PATH/get/get_device_param $device_index upstream_port)
-bdf=$(echo "$upstream_port" | sed 's/0$/1/')
-
-#validate
-/opt/xilinx/xrt/bin/xbutil validate --device $bdf
-
-echo ""
