706e0611677276b7a22d7bc74136e7f60a0aef3e
Javier <javier@staff-net-vpn-dhcp-7125.intern.ethz.ch>
2023-06-07 16:25:30 +0200

    Cleanup

--- cli/program/coyote.sh
+++ cli/program/coyote.sh
@@ -47,8 +47,6 @@ fi
 #inputs
 read -a flags <<< "$@"
 
-#----------------------------------------------------------
-
 #check on flags
 project_found=""
 project_name=""
@@ -169,75 +167,8 @@ else
     fi
 fi
 
-echo $project_found
-echo $project_name
-echo $device_found
-echo $device_index
-echo $deploy_option
-
-#exit
-
-#-------------
-
-
-
-
 echo ""
 echo "${bold}sgutil program coyote${normal}"
-
-##check on flags (before: flags cannot be empty)
-#name_found="0"
-#project_found="0"
-##serial_found="0"
-#if [ "$flags" = "" ]; then
-#    #no flags: start dialog
-#    cd /home/$username/my_projects/coyote/
-#    projects=( *"/" )
-#    #delete validate folders from projects
-#    j=0
-#    for i in "${projects[@]}"
-#    do
-#        if [[ $i =~ validate_* ]]; then
-#            echo "" >&/dev/null
-#        else
-#            aux[j]=$i
-#            j=$(($j + 1))
-#        fi
-#    done
-#    echo ""
-#    echo "${bold}Please, choose your project:${normal}"
-#    echo ""
-#    PS3=""
-#    select project_name in "${aux[@]}"; do
-#        if [[ -z $project_name ]]; then
-#            echo "" >&/dev/null
-#        else
-#            project_found="1"
-#            project_name=${project_name::-1} #we remove the last character, i.e. "/""
-#            break
-#        fi
-#    done
-#else
-#    #find flags and values
-#    for (( i=0; i<${#flags[@]}; i++ ))
-#    do
-#        if [[ " ${flags[$i]} " =~ " -n " ]] || [[ " ${flags[$i]} " =~ " --name " ]]; then 
-#            name_found="1"
-#            name_idx=$(($i+1))
-#            device_name=${flags[$name_idx]}
-#        fi
-#        if [[ " ${flags[$i]} " =~ " -p " ]] || [[ " ${flags[$i]} " =~ " --project " ]]; then
-#            project_found="1"
-#            project_idx=$(($i+1))
-#            project_name=${flags[$project_idx]}
-#        fi
-#    done
-#    #forbidden combinations
-#    if [[ $project_found = "0" ]] || ([ "$project_found" = "1" ] && [ "$project_name" = "" ]) || ([ $project_found = "0" ] && [ $name_found = "1" ]) || ([ "$name_found" = "1" ] && [ "$device_name" = "" ]); then
-#        $CLI_PATH/sgutil build coyote -h
-#        exit
-#    fi
-#fi
 
 #define directories (1)
 DIR="/home/$username/my_projects/$WORKFLOW/$project_name"
@@ -272,11 +203,7 @@ cd $APP_BUILD_DIR
 #prgramming local server
 echo "Programming local server ${bold}$hostname...${normal}"
 #bitstream and driver
-#$CLI_PATH/sgutil program vivado --device $device_index -b $APP_BUILD_DIR$BIT_NAME --driver $APP_BUILD_DIR$DRIVER_NAME
-#$CLI_PATH/sgutil program vivado --device $device_index -b $APP_BUILD_DIR$BIT_NAME --driver $DRIVER_NAME
 $CLI_PATH/sgutil program vivado --device $device_index -b $BIT_NAME --driver $DRIVER_NAME
-#driver 
-#$CLI_PATH/sgutil program vivado -d $APP_BUILD_DIR$DRIVER_NAME
 #get permissions on N_REGIONS
 $CLI_PATH/program/get_N_REGIONS $DIR
 
