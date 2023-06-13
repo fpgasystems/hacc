f0a63f9bc60b147ca256f8f3b7f5ad46a41f8526
Javier <javier@staff-net-vpn-dhcp-0437.intern.ethz.ch>
2023-06-13 16:20:10 +0200

    Check for one or multiple projects

--- cli/common/project_dialog.sh
+++ cli/common/project_dialog.sh
@@ -6,8 +6,9 @@ workflow=$2
 # Declare global variables
 declare -g project_found="0"
 declare -g project_name=""
+declare -g multiple_projects="0"
 
-# no flags: start dialog
+#get projects
 cd /home/$username/my_projects/$workflow/
 projects=( *"/" )
 
@@ -23,22 +24,27 @@ do
     fi
 done
 
-#echo ""
-#echo "${bold}Please, choose your project:${normal}"
-#echo ""
-PS3=""
+# Check if there is only one directory
+if [ ${#aux[@]} -eq 1 ]; then
+    project_found="1"
+    project_name=${aux[0]}
+    project_name=${project_name::-1} # remove the last character, i.e. "/"
+else
+    multiple_projects="1"
+    PS3=""
+    select project_name in "${aux[@]}"; do
+        if [[ -z $project_name ]]; then
+            echo "" >&/dev/null
+        else
+            project_found="1"
+            project_name=${project_name::-1} # remove the last character, i.e. "/"
+            break
+        fi
+    done
 
-# Capture user selection into the project_name variable
-select project_name in "${aux[@]}"; do
-    if [[ -z $project_name ]]; then
-        echo "" >&/dev/null
-    else
-        project_found="1"
-        project_name=${project_name::-1} # remove the last character, i.e. "/"
-        break
-    fi
-done
+fi
 
 # Return the values of project_found and project_name
 echo "$project_found"
 echo "$project_name"
+echo "$multiple_projects"
