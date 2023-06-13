2383d6de64944a15e5bd4551c54d6d3141a2d1f0
Javier <javier@staff-net-als-2082.intern.ethz.ch>
2023-06-13 14:52:30 +0200

    multiple_platforms

--- cli/common/platform_dialog.sh
+++ cli/common/platform_dialog.sh
@@ -5,22 +5,32 @@ XILINX_PATH=$1
 # Declare global variables
 declare -g platform_found="0"
 declare -g platform_name=""
+declare -g multiple_platforms="0"
 
 #get platforms
-cd $XILINX_PATH/platforms
+cd "$XILINX_PATH/platforms"
 platforms=( "xilinx_"* )
 
-PS3=""
-select platform_name in "${platforms[@]}"; do 
-    if [[ -z $platform_name ]]; then
-        echo "" >&/dev/null
-    else
-        platform_found="1"
-        platform_name=${platform_name::-1} # remove the last character, i.e. "/"
-        break
-    fi
-done
+# Check if there is only one directory
+if [ ${#platforms[@]} -eq 1 ]; then
+    platform_found="1"
+    platform_name=${platforms[0]}
+    #platform_name=${platform_name::-1} # remove the last character, i.e. "/"
+else
+    multiple_platforms="1"
+    PS3=""
+    select platform_name in "${platforms[@]}"; do 
+        if [[ -z $platform_name ]]; then
+            echo "" >&/dev/null
+        else
+            platform_found="1"
+            #platform_name=${platform_name::-1} # remove the last character, i.e. "/"
+            break
+        fi
+    done
+fi
 
 # Return values
 echo "$platform_found"
 echo "$platform_name"
+echo "$multiple_platforms"
