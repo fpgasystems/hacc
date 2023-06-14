b3d27dad8400bb1d8af91dcb81de724cbc2a1cdd
Javier <javier@staff-net-vpn-dhcp-0336.intern.ethz.ch>
2023-06-14 10:15:12 +0200

    device_name_dialog_check add

---
+++ cli/common/device_name_dialog_check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+flags=("$@")  # Assign command-line arguments to the 'flags' array
+
+# Declare global variables
+declare -g device_found="0"
+declare -g device_name=""
+
+#read flags
+for (( i=0; i<${#flags[@]}; i++ ))
+do
+    if [[ " ${flags[$i]} " =~ " -d " ]] || [[ " ${flags[$i]} " =~ " --device " ]]; then # flags[i] is -d or --device
+        device_found="1"
+        device_idx=$(($i+1))
+        device_name=${flags[$device_idx]}
+    fi  
+done
+
+#return the values
+echo "$device_found"
+echo "$device_name"
