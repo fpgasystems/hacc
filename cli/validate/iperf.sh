d272e6891a5c298022946f4cd5a9219db663affd
Javier <javier@staff-net-vpn-dhcp-0097.intern.ethz.ch>
2023-06-19 08:28:43 +0200

    Validated with U55c-01 02 and all hacc boxes

--- cli/validate/iperf.sh
+++ cli/validate/iperf.sh
@@ -3,27 +3,25 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+#constants
+CLI_PATH="/opt/cli"
+
 echo ""
-#echo "${bold}iperf${normal}"
-#echo ""
 
-# constants
-CLI_WORKDIR="/opt/cli"
+#setup keys
+eval "$CLI_PATH/common/ssh_key_add"
 
-# setup keys
-eval "$CLI_WORKDIR/common/ssh_key_add"
-
-# get hostname
+#get hostname
 url="${HOSTNAME}"
 hostname="${url%%.*}"
 
-# inputs
+#inputs
 flags=$@
 
-# replace p by P
+#replace p by P
 flags=${flags/p/P}
 
-# set default
+#set default
 udp_server=""
 if [ "$flags" = "" ]; then
     flags="-P 4"
@@ -32,7 +30,7 @@ elif [ "$flags" = "-u" ]; then
     udp_server=" -u"
 fi
 
-# start iperf server on local machine
+#start iperf server on local machine
 echo "${bold}Starting iperf server:${normal}"
 echo ""
 echo "iperf -s -B $hostname-mellanox-0 -D $udp_server"
@@ -40,14 +38,14 @@ echo ""
 iperf -s -B $hostname-mellanox-0 -D $udp_server
 echo ""
 
-# get booked machines
+#get booked machines
 servers=$(sudo /opt/cli/common/get_booking_system_servers_list | tail -n +2)
 echo ""
 
-# convert string to an array
+#convert string to an array
 servers=($servers)
 
-# running iperf on remote machines
+#running iperf on remote machines
 echo "${bold}Running iperf on remote server/s:${normal}"
 echo ""
 for i in "${servers[@]}"
@@ -59,8 +57,3 @@ do
         echo ""
     fi
 done
-
-# stop iperf server on local machine
-#echo "${bold}Stoping iperf server:${normal}"
-#echo ""
-#pkill iperf
