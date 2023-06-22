9db4e0d089687fb191b8270b734dc04225c014aa
Javier <javier@staff-net-vpn-dhcp-0750.intern.ethz.ch>
2023-06-22 06:11:48 +0200

    Help fix

--- cli/get/workflow.sh
+++ cli/get/workflow.sh
@@ -46,7 +46,7 @@ else
     device_index=$(echo "$result" | sed -n '2p')
     #forbidden combinations
     if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then
-        $CLI_PATH/sgutil get platform -h
+        $CLI_PATH/sgutil get workflow -h
         exit
     fi
     #device_dialog (forgotten mandatory)
@@ -54,7 +54,7 @@ else
         device_found="1"
         device_index="1"
     elif [[ $device_found = "0" ]]; then
-        $CLI_PATH/sgutil get platform -h
+        $CLI_PATH/sgutil get workflow -h
         exit
     fi
     #get Bus Device Function
