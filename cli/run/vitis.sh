7e53e4e7d820ae891dad94f4f8c39d056684a5a7
Javier <javier@staff-net-vpn-dhcp-7930.intern.ethz.ch>
2023-06-26 13:23:58 +0200

    add BUILD_SERVERS_LIST

--- cli/run/vitis.sh
+++ cli/run/vitis.sh
@@ -6,11 +6,23 @@ normal=$(tput sgr0)
 #constants
 CLI_PATH="/opt/cli"
 HACC_PATH="/opt/hacc"
+BUILD_SERVERS_LIST="$CLI_PATH/constants/BUILD_SERVERS_LIST"
 DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
 WORKFLOW="vitis"
 
 #get username
 username=$USER
+
+#get hostname
+url="${HOSTNAME}"
+hostname="${url%%.*}"
+
+#check on build server
+if grep -q "^$hostname$" $BUILD_SERVERS_LIST; then
+    echo ""
+    echo "Sorry, this command is not available on ${bold}$hostname!${normal}"
+    exit
+fi
 
 #check on DEVICES_LIST
 source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
