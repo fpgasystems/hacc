6f4ab63dfe1319e668169ffee9576029a5f8629c
Javier <javier@staff-net-vpn-dhcp-1037.intern.ethz.ch>
2023-06-28 09:41:46 +0200

    Using function instead

--- cli/validate/coyote.sh
+++ cli/validate/coyote.sh
@@ -108,14 +108,7 @@ else
 fi
 
 #get vivado_devices
-vivado_devices=0
-for ((i=1; i<=$MAX_DEVICES; i++)); do
-    workflow=$($CLI_PATH/get/workflow -d $i)
-    workflow=$(echo "$workflow" $i | cut -d' ' -f2 | sed '/^\s*$/d')
-    if [ "$workflow" = "vivado" ] && [ "$i" -ne $device_index ]; then
-        ((vivado_devices++))
-    fi 
-done
+vivado_devices=$($CLI_PATH/common/get_vivado_devices $CLI_PATH $MAX_DEVICES $device_index)
 
 #check on VIVADO_DEVICES_MAX
 if [ $vivado_devices -ge $((VIVADO_DEVICES_MAX)) ]; then
