dce3c34bea59bf17accd1d65e3aefb1646e39c98
Javier <javier@staff-net-vpn-dhcp-0830.intern.ethz.ch>
2023-06-29 17:21:25 +0200

    Cleanup

--- cli/common/is_virtualized.sh
+++ cli/common/is_virtualized.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#get hostname
-#url="${HOSTNAME}"
-#hostname="${url%%.*}"
-
 #inputs
 CLI_PATH=$1
 hostname=$2
@@ -11,16 +7,11 @@ hostname=$2
 #constants
 VIRTUALIZED_SERVERS_LIST="$CLI_PATH/constants/VIRTUALIZED_SERVERS_LIST"
 
+#check for virtualized
 virtualized="0"
 if (grep -q "^$hostname$" $VIRTUALIZED_SERVERS_LIST); then
     virtualized="1"
 fi
-echo $virtualized
 
-#chech if virtualized
-#if [ "$hostname" = "alveo-u250-01" ] || [ "$hostname" = "alveo-u250-02" ] || [ "$hostname" = "alveo-u250-03" ] || [ "$hostname" = "alveo-u250-04" ] || [ "$hostname" = "alveo-u250-05" ] || [ "$hostname" = "alveo-u250-06" ] || [ "$hostname" = "alveo-u280-01" ] || [ "$hostname" = "alveo-u280-02" ] || [ "$hostname" = "alveo-u280-03" ] || [ "$hostname" = "alveo-u280-04" ] || [ "$hostname" = "versal-vck5000-01" ]; then
-#    #virtualized environment
-#    echo "true"
-#else
-#    echo "false"
-#fi
+#output
+echo $virtualized
