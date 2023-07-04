fb180efdb9e84ba661b47a0ec110748ed868a3fc
Javier <javier@staff-net-als-0862.intern.ethz.ch>
2023-07-04 12:50:04 +0200

    CLI_PATH add

--- cli/common/get_booking_system_servers_list.sh
+++ cli/common/get_booking_system_servers_list.sh
@@ -3,7 +3,10 @@
 username=$(getent passwd ${SUDO_UID})
 username=${username%%:*}
 
-DB_IP=$(/opt/cli/common/get_booking_system_host) 
-DB_USER=$(/opt/cli/common/get_booking_system_db_user) 
-DB_PASS=$(/opt/cli/common/get_booking_system_db_passwd) 
+#constants
+CLI_PATH=$(../common/get_path ../ CLI_PATH)
+
+DB_IP=$($CLI_PATH/common/get_booking_system_host) 
+DB_USER=$($CLI_PATH/common/get_booking_system_db_user) 
+DB_PASS=$($CLI_PATH/common/get_booking_system_db_passwd) 
 mysql -h $DB_IP -u $DB_USER -D alveo -p$DB_PASS -e "SELECT server.name FROM booking INNER JOIN booking_map ON booking.id = booking_map.booking_id INNER JOIN server ON booking_map.server_id = server.id WHERE now() BETWEEN booking.begin AND booking.end AND booking.status=1 AND user='$username';"
