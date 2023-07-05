a6ad748e93a67d094e58589ecc03e1da76ee0167
Javier <javier@staff-net-vpn-dhcp-1753.intern.ethz.ch>
2023-07-05 18:22:13 +0200

    Verified version

--- cli/common/version_dialog.sh
+++ cli/common/version_dialog.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 VIVADO_PATH=$1
 
 # Declare global variables
 declare -g version_found="0"
 declare -g version_name=""
 
-#get projects
-cd /home/$username/my_projects/$workflow/
-projects=( *"/" )
-
 #get installed versions
 cd $VIVADO_PATH
-versions=( *"/" )
-
-#get installed versions
-cd $VIVADO_PATH
-versions=( *"/" )
+versions=($(ls -d */))
 
 #check on number of versions
 if [ ${#versions[@]} -eq 1 ]; then
-    version=${versions[0]}
+    version_found="1"
+    version_name=${versions[0]}
     #version=${version::-1} # remove the last character, i.e. "/"
 else
     echo "${bold}Please, choose your Xilinx release branch:${normal}"
     echo ""
     PS3=""
     #versions+=("none")
-    select version in "${versions[@]}"; do
-        if [[ -z $version ]]; then
+    select version_name in "${versions[@]}"; do
+        if [[ -z $version_name ]]; then
             echo "" >&/dev/null
         else
+            version_found="1"
             break
         fi
     done
