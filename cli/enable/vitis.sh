b397a69428d3c9c6b154b93af51e5257a59f6572
Javier <javier@staff-net-vpn-dhcp-7846.intern.ethz.ch>
2023-07-07 05:29:17 +0200

    Update verified

--- cli/enable/vitis.sh
+++ cli/enable/vitis.sh
@@ -4,52 +4,20 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 #constants
-CLI_PATH="$(dirname "$(dirname "$0")")"
+CLI_PATH="/opt/cli" #"$(dirname "$(dirname "$0")")"
 HACC_PATH="/opt/hacc"
 XRT_PATH="/opt/xilinx/xrt"
-DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
-MY_PROJECTS_PATH=$($CLI_PATH/common/get_path $CLI_PATH MY_PROJECTS_PATH)
-WORKFLOW="vitis"
-TARGET="hw"
-
-echo "This is enable/vitis"
-exit
+XILINX_TOOLS_PATH="/tools/Xilinx"
+VITIS_PATH="$XILINX_TOOLS_PATH/Vitis"
 
 #get hostname
 url="${HOSTNAME}"
 hostname="${url%%.*}"
 
-#check on ACAP or FPGA servers (server must have at least one ACAP or one FPGA)
-acap=$($CLI_PATH/common/is_acap $CLI_PATH $hostname)
-fpga=$($CLI_PATH/common/is_fpga $CLI_PATH $hostname)
-if [ "$acap" = "0" ] && [ "$fpga" = "0" ]; then
-    echo ""
-    echo "Sorry, this command is not available on ${bold}$hostname!${normal}"
-    echo ""
-    exit
-fi
-
 #check on valid XRT version
-if [ ! -d $XRT_PATH ]; then #if [ -z "$(echo $XILINX_XRT)" ]; then
+if [ -n "$XILINX_VITIS" ]; then
     echo ""
-    echo "Please, source a valid XRT and Vitis version for ${bold}$hostname!${normal}"
-    echo ""
-    exit 1
-fi
-
-#check on DEVICES_LIST
-source "$CLI_PATH/common/device_list_check" "$DEVICES_LIST"
-
-#get number of fpga and acap devices present
-MAX_DEVICES=$(grep -E "fpga|acap" $DEVICES_LIST | wc -l)
-
-#check on multiple devices
-multiple_devices=$($CLI_PATH/common/get_multiple_devices $MAX_DEVICES)
-
-#check if workflow exists
-if ! [ -d "$MY_PROJECTS_PATH/$WORKFLOW/" ]; then
-    echo ""
-    echo "You must create build your project first! Please, use sgutil build vitis"
+    echo "Vitis is already active on ${bold}$hostname!${normal}"
     echo ""
     exit
 fi
@@ -58,187 +26,62 @@ fi
 read -a flags <<< "$@"
 
 #check on flags
-project_found=""
-project_name=""
-device_found=""
-device_index=""
+version_found=""
+version_name=""
 if [ "$flags" = "" ]; then
-    #header (1/2)
+    #header
     echo ""
-    echo "${bold}sgutil program vitis${normal}"
-    #project_dialog
+    echo "${bold}sgutil enable xrt${normal}"
+    #version_dialog
     echo ""
-    echo "${bold}Please, choose your $WORKFLOW project:${normal}"
+    echo "${bold}Please, choose your XRT version:${normal}"
     echo ""
-    result=$($CLI_PATH/common/project_dialog $USER $WORKFLOW)
-    project_found=$(echo "$result" | sed -n '1p')
-    project_name=$(echo "$result" | sed -n '2p')
-    multiple_projects=$(echo "$result" | sed -n '3p')
-    if [[ $multiple_projects = "0" ]]; then
-        echo $project_name
-    fi
-    #device_dialog
-    if [[ $multiple_devices = "0" ]]; then
-        device_found="1"
-        device_index="1"
-    else
-        echo ""
-        echo "${bold}Please, choose your device:${normal}"
-        echo ""
-        result=$($CLI_PATH/common/device_dialog $CLI_PATH $MAX_DEVICES $multiple_devices)
-        device_found=$(echo "$result" | sed -n '1p')
-        device_index=$(echo "$result" | sed -n '2p')
-    fi
-    #get_servers
+    result=$($CLI_PATH/common/version_dialog $VITIS_PATH)
+    version_found=$(echo "$result" | sed -n '1p')
+    version_name=$(echo "$result" | sed -n '2p')
     echo ""
-    result=$($CLI_PATH/common/get_servers $CLI_PATH $hostname)
-    servers_family_list=$(echo "$result" | sed -n '1p' | sed -n '1p')
-    servers_family_list_string=$(echo "$result" | sed -n '2p' | sed -n '1p')
-    num_remote_servers=$(echo "$servers_family_list" | wc -w)
-    echo ""
-    #deployment_dialog
-    deploy_option="0"
-    if [ "$num_remote_servers" -ge 1 ]; then
-        echo "${bold}Please, choose your deployment servers:${normal}"
-        echo ""
-        echo "0) $hostname"
-        echo "1) $hostname, $servers_family_list_string"
-        deploy_option=$($CLI_PATH/common/deployment_dialog $servers_family_list_string)
-        echo ""
-    fi
 else
-    #project_dialog_check
-    result="$("$CLI_PATH/common/project_dialog_check" "${flags[@]}")"
-    project_found=$(echo "$result" | sed -n '1p')
-    project_name=$(echo "$result" | sed -n '2p')
+    #version_dialog_check
+    result="$("$CLI_PATH/common/version_dialog_check" "${flags[@]}")"
+    version_found=$(echo "$result" | sed -n '1p')
+    version_name=$(echo "$result" | sed -n '2p')
     #forbidden combinations
-    if [ "$project_found" = "1" ] && ([ "$project_name" = "" ] || [ ! -d "$MY_PROJECTS_PATH/$WORKFLOW/$project_name" ]); then 
-        $CLI_PATH/sgutil program vitis -h
+    if [ "$version_found" = "1" ] && ([ "$version_name" = "" ] || [ ! -d "$VITIS_PATH/$version_name" ]); then 
+        $CLI_PATH/sgutil enable xrt -h
         exit
-    fi
-    #device_dialog_check
-    result="$("$CLI_PATH/common/device_dialog_check" "${flags[@]}")"
-    device_found=$(echo "$result" | sed -n '1p')
-    device_index=$(echo "$result" | sed -n '2p')
-    #forbidden combinations
-    if ([ "$device_found" = "1" ] && [ "$device_index" = "" ]) || ([ "$device_found" = "1" ] && [ "$multiple_devices" = "0" ] && (( $device_index != 1 ))) || ([ "$device_found" = "1" ] && ([[ "$device_index" -gt "$MAX_DEVICES" ]] || [[ "$device_index" -lt 1 ]])); then
-        $CLI_PATH/sgutil program vitis -h
-        exit
-    fi
-    #deployment_dialog_check
-    result="$("$CLI_PATH/common/deployment_dialog_check" "${flags[@]}")"
-    deploy_option_found=$(echo "$result" | sed -n '1p')
-    deploy_option=$(echo "$result" | sed -n '2p')
-    #forbidden combinations
-    if [ "$deploy_option_found" = "1" ] && { [ "$deploy_option" -ne 0 ] && [ "$deploy_option" -ne 1 ]; }; then #if [ "$deploy_option_found" = "1" ] && [ -n "$deploy_option" ]; then 
-        $CLI_PATH/sgutil program vitis -h
-        exit
-    fi
-    #header (2/2)
-    echo ""
-    echo "${bold}sgutil program vitis${normal}"
-    echo ""
-    #project_dialog (forgotten mandatory 1)
-    if [[ $project_found = "0" ]]; then
-        #echo ""
-        echo "${bold}Please, choose your $WORKFLOW project:${normal}"
-        echo ""
-        result=$($CLI_PATH/common/project_dialog $USER $WORKFLOW)
-        project_found=$(echo "$result" | sed -n '1p')
-        project_name=$(echo "$result" | sed -n '2p')
-        multiple_projects=$(echo "$result" | sed -n '3p')
-        if [[ $multiple_projects = "0" ]]; then
-            echo $project_name
-        fi
-        #echo ""
-    fi
-    #device_dialog (forgotten mandatory 2)
-    if [[ $multiple_devices = "0" ]]; then
-        device_found="1"
-        device_index="1"
-    elif [[ $device_found = "0" ]]; then
-        echo "${bold}Please, choose your device:${normal}"
-        echo ""
-        result=$($CLI_PATH/common/device_dialog $CLI_PATH $MAX_DEVICES $multiple_devices)
-        device_found=$(echo "$result" | sed -n '1p')
-        device_index=$(echo "$result" | sed -n '2p')
-        echo ""
-    fi
-    #get_servers
-    result=$($CLI_PATH/common/get_servers $CLI_PATH $hostname)
-    servers_family_list=$(echo "$result" | sed -n '1p' | sed -n '1p')
-    servers_family_list_string=$(echo "$result" | sed -n '2p' | sed -n '1p')
-    num_remote_servers=$(echo "$servers_family_list" | wc -w)
-    echo ""
-    #deployment_dialog (forgotten mandatory 3)
-    if [ "$deploy_option_found" = "0" ]; then
-        #deployment_dialog
-        deploy_option="0"
-        if [ "$num_remote_servers" -ge 1 ]; then
-            echo "${bold}Please, choose your deployment servers:${normal}"
-            echo ""
-            echo "0) $hostname"
-            echo "1) $hostname, $servers_family_list_string"
-            deploy_option=$($CLI_PATH/common/deployment_dialog $servers_family_list_string)
-            echo ""
-        fi
     fi
 fi
 
-#define directories (1)
-DIR="$MY_PROJECTS_PATH/$WORKFLOW/$project_name"
+#copy the desired XRT version to user’s local and preserve /opt/xilinx/xrt structure (Xilinx workaroud)
+#mkdir -p /local/home/$USER/xrt_${version_name}$XRT_PATH
+#cp -r $XRT_PATH"_"${version_name}/* /local/home/$USER/xrt_${version_name}$XRT_PATH 
 
-#check if project exists
-if ! [ -d "$DIR" ]; then
+#source vitis
+source $XILINX_TOOLS_PATH//Vitis/2022.1/.settings64-Vitis.sh
+source $XILINX_TOOLS_PATH//Vitis_HLS/2022.1/.settings64-Vitis_HLS.sh
+export XILINX_VITIS=$VITIS_PATH/$version_name
+
+#echo ""
+
+#print message
+echo ""
+if [[ -d $VITIS_PATH/$version_name ]]; then
+    #Vitis is installed
+    echo "The server is ready to work with ${bold}Vitis $version_name${normal} release branch:"
     echo ""
-    echo "$DIR is not a valid --project name!"
+    #echo "    Xilinx Board Utility (xbutil)          : ${bold}$XILINX_XRT/bin${normal}"
+    echo "    Xilinx Tools (Vitis, Vitis_HLS)        : ${bold}$XILINX_TOOLS_PATH${normal}"
+#elif [[ -d $VITIS_PATH/$version_name ]]; then
+#    #Vitis is not installed
+#    echo "The server is ready to work with Xilinx ${bold}$version_name${normal} release branch:"
+#    echo ""
+#    echo "    Xilinx Board Utility (xbutil)       : ${bold}$XILINX_XRT/bin${normal}"
+#    echo "    Xilinx Tools (Vivado, Vitis_HLS)    : ${bold}$XILINX_TOOLS_PATH${normal}"
+else
+    echo "The server needs special care to operate with XRT normally (Xilinx tools are not properly installed)."
     echo ""
-    exit
-fi
-
-#get platform
-platform=$($CLI_PATH/get/get_fpga_device_param $device_index platform)
-
-#define directories (2)
-APP_BUILD_DIR="$MY_PROJECTS_PATH/$WORKFLOW/$project_name/build_dir.$TARGET.$platform"
-
-#check for build directory
-if ! [ -d "$APP_BUILD_DIR" ]; then
-    echo ""
-    echo "You must build your project first! Please, use sgutil build vitis (target = hw)"
-    echo ""
-    exit
-fi
-
-#get xclbin
-cd $APP_BUILD_DIR
-xclbin=$(echo *.xclbin | awk '{print $NF}')
-
-#get BDF (i.e., Bus:Device.Function) 
-upstream_port=$($CLI_PATH/get/get_fpga_device_param $device_index upstream_port)
-bdf="${upstream_port%?}1"
-
-#programming local server
-echo "Programming local server ${bold}$hostname...${normal}"
-#revert to xrt first if FPGA is already in baremetal
-sudo $CLI_PATH/program/revert -d $device_index
-#reset device (we delete any xclbin)
-$XRT_PATH/bin/xbutil reset --device $bdf --force
-#program xclbin
-$XRT_PATH/bin/xbutil program --device $bdf -u $xclbin
-
-#programming remote servers
-if [ "$deploy_option" -eq 1 ]; then 
-    #convert string to array
-    IFS=" " read -ra servers_family_list_array <<< "$servers_family_list"
-    for i in "${servers_family_list_array[@]}"; do
-        #remote servers
-        echo ""
-        echo "Programming remote server ${bold}$i...${normal}"
-        echo ""
-        #remotely revert to xrt, reset device (delete any xclbin), and program xclbin
-        ssh -t "$USER@$i" "sudo $CLI_PATH/program/revert -d $device_index; $XRT_PATH/bin/xbutil reset --device $bdf --force; $XRT_PATH/bin/xbutil program --device $bdf -u $APP_BUILD_DIR/$xclbin"
-    done
+    echo "${bold}An email has been sent to the person in charge;${normal} we will let you know when XRT is ready to use again."
+    echo "Subject: $hostname requires special attention ($username): Xilinx tools are not properly installed" | sendmail $email
 fi
 
 echo ""
