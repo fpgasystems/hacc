ae0582554d035e9b4428a040ed3a5583661b3b40
Javier <javier@staff-net-vpn-dhcp-2970.intern.ethz.ch>
2023-07-21 08:55:29 +0200

    devices use CLI_PATH instead of HACC_PATH

--- cli/validate/coyote.sh
+++ cli/validate/coyote.sh
@@ -5,11 +5,10 @@ normal=$(tput sgr0)
 
 #constants
 CLI_PATH="$(dirname "$(dirname "$0")")"
-HACC_PATH="/opt/hacc"
 XILINX_TOOLS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH XILINX_TOOLS_PATH)
 VIVADO_PATH="$XILINX_TOOLS_PATH/Vivado"
 VIVADO_DEVICES_MAX=$(cat $CLI_PATH/constants/VIVADO_DEVICES_MAX)
-DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
+DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
 MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
 WORKFLOW="coyote"
 BIT_NAME="cyt_top.bit"
