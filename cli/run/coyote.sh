633d8ef6b6f84dfc6cd918434c7dbd9eb66104fe
Javier <javier@staff-net-vpn-dhcp-2970.intern.ethz.ch>
2023-07-21 08:52:14 +0200

    Using CLI_PATH instead of HACC_PATH for devices

--- cli/run/coyote.sh
+++ cli/run/coyote.sh
@@ -5,8 +5,7 @@ normal=$(tput sgr0)
 
 #constants
 CLI_PATH="$(dirname "$(dirname "$0")")"
-HACC_PATH="/opt/hacc"
-DEVICES_LIST="$HACC_PATH/devices_reconfigurable"
+DEVICES_LIST="$CLI_PATH/devices_acap_fpga"
 MY_PROJECTS_PATH=$($CLI_PATH/common/get_constant $CLI_PATH MY_PROJECTS_PATH)
 WORKFLOW="coyote"
 
