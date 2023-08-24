097bd1587e209b618b47607ac103e060f5fa82fc
Javier <javier@staff-net-vpn-dhcp-7437.intern.ethz.ch>
2023-08-24 08:28:35 +0200

    get_files add

---
+++ cli/common/get_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIR=$1 
+EXTENSION=$2 
+
+# Declare global variables
+declare -g file_list=""
+
+# Use find command to locate all .cpp files in the specified directory
+# and loop through the results
+while IFS= read -r -d $'\0' file; do
+    # Append the filename to the file_list string
+    file_list="${file_list}${file} "
+done < <(find "$DIR" -type f -name "*$EXTENSION" -print0)
+
+#return list
+echo "$file_list"
